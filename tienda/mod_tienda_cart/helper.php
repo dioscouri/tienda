8ebf7d2739b9639b112ddd63a5071d24db14679a
Daniele Rosario <realskullbock@gmail.com>
2010-03-02 13:58:28 +0000

    Moved inline cart styles to a separate css, refs #31 and closes #63

--- tienda/mod_tienda_cart/helper.php
+++ tienda/mod_tienda_cart/helper.php
@@ -8,25 +8,23 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
 */
 
-
 /** ensure this file is being included by a parent file */
 defined('_JEXEC') or die('Restricted access');
 
-class modTiendaUserAddressHelper
+class modTiendaCartHelper
 {
-    function getAddresses()
+    function getCart()
     {
     	//require_once( JPATH_ADMINISTRATOR.DS.'components'.DS.'com_tienda'.DS.'defines.php' );
-        JLoader::import( 'com_tienda.helpers.user', JPATH_ADMINISTRATOR.DS.'components' );
+        JLoader::import( 'com_tienda.helpers.carts', JPATH_ADMINISTRATOR.DS.'components' );
         JTable::addIncludePath( JPATH_ADMINISTRATOR.DS.'components'.DS.'com_tienda'.DS.'tables' );
         JModel::addIncludePath( JPATH_SITE.DS.'components'.DS.'com_tienda'.DS.'models' );
 
         // determine whether we're working with a session or db cart
-        //$userAddress = TiendaHelperUser::getPrimaryAddress(JRequest::getVar('id', 0));
-    	$model = JModel::getInstance( 'Addresses', 'TiendaModel' );
-    	$model->setState('filter_userid', JRequest::getVar('id', 0));
-    	$userAddresses = $model->getList();
-    	return $userAddresses;
+        $suffix = TiendaHelperCarts::getSuffix();
+    	$model = JModel::getInstance( $suffix, 'TiendaModel' );
+    	$cart = $model->getList();
+    	return $cart;
     }
 }
 ?>
