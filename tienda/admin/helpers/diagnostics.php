2a7b33d45f4bf6211b84b8733a0b9491f375a4b3
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2010-03-05 21:29:27 +0000

    Added check to _productfiles to the diagnostic helper, fixes #11

--- tienda/admin/helpers/diagnostics.php
+++ tienda/admin/helpers/diagnostics.php
@@ -47,6 +47,13 @@ class TiendaHelperDiagnostics extends TiendaHelperBase
         {
             return $this->redirect( JText::_('DIAGNOSTIC CHECKDEFAULTCURRENCY FAILED') .' :: '. $this->getError(), 'error' );
         }
+        
+        // check the productfiles table 
+        // TODO deprecate this check eventually, b/c it is only needed it the admin installed 0.2.0
+        if (!$this->checkProductFiles()) 
+        {
+            return $this->redirect( JText::_('DIAGNOSTIC CHECKPRODUCTFILES FAILED') .' :: '. $this->getError(), 'error' );
+        }
     }
     
     /**
@@ -87,10 +94,49 @@ class TiendaHelperDiagnostics extends TiendaHelperBase
         return true;
     }
     
+    /**
+     * Changes fields in a table
+     * 
+     * @param string $table
+     * @param array $fields
+     * @param array $definitions
+     * @param array $newnames
+     * @return boolean
+     */
+    function changeTableFields($table, $fields, $definitions, $newnames)
+    {
+        $database = JFactory::getDBO();
+        $fields = (array) $fields;
+        $errors = array();
+        
+        foreach ($fields as $field)
+        {
+            $query = " SHOW COLUMNS FROM {$table} LIKE '{$field}' ";
+            $database->setQuery( $query );
+            $rows = $database->loadObjectList();
+            if ($rows && !$database->getErrorNum()) 
+            {       
+                $query = "ALTER TABLE `{$table}` CHANGE `{$field}` `{$newnames[$field]}` {$definitions[$field]}; ";
+                $database->setQuery( $query );
+                if (!$database->query())
+                {
+                    $errors[] = $database->getErrorMsg();
+                }
+            }
+        }
+        
+        if (!empty($errors))
+        {
+            $this->setError( implode('<br/>', $errors) );
+            return false;
+        }
+        return true;
+    }
+    
 	/**
 	 * Check if a default currencies has been selected,
 	 * and if the selected currency really exists
-	 * @return unknown_type
+	 * @return boolean
 	 */
 	function checkDefaultCurrency() 
 	{
@@ -113,5 +159,70 @@ class TiendaHelperDiagnostics extends TiendaHelperBase
         }
         return true;
 	}
+	
+    /**
+     * Check if the _productfiles table is correct
+     * This is only necessary if 0.2.0 was ever installed
+     * 
+     * @return boolean
+     */
+    function checkProductFiles() 
+    {
+        // if this has already been done, don't repeat
+        if (TiendaConfig::getInstance()->get('checkProductFiles', '0'))
+        {
+            return true;
+        }
+        
+        $table = '#__tienda_productfiles';
+        $definitions = array();
+        $fields = array();
+        
+        $fields[] = "file_id";
+            $newnames["file_id"] = "productfile_id";
+            $definitions["file_id"] = "int(11) NOT NULL AUTO_INCREMENT";
+
+        $fields[] = "file_name";
+            $newnames["file_name"] = "productfile_name";
+            $definitions["file_name"] = "varchar(128) NOT NULL DEFAULT ''";
+
+        $fields[] = "file_path";
+            $newnames["file_path"] = "productfile_path";
+            $definitions["file_path"] = "varchar(255) NOT NULL";
+            
+        $fields[] = "file_description";
+            $newnames["file_description"] = "productfile_description";
+            $definitions["file_description"] = "mediumtext NOT NULL";
+            
+        $fields[] = "file_extension";
+            $newnames["file_extension"] = "productfile_extension";
+            $definitions["file_extension"] = "varchar(6) NOT NULL DEFAULT ''";
+            
+        $fields[] = "file_mimetype";
+            $newnames["file_mimetype"] = "productfile_mimetype";
+            $definitions["file_mimetype"] = "varchar(64) NOT NULL DEFAULT ''";
+            
+        $fields[] = "file_url";
+            $newnames["file_url"] = "productfile_url";
+            $definitions["file_url"] = "varchar(255) NOT NULL DEFAULT ''";
+            
+        $fields[] = "file_enabled";
+            $newnames["file_enabled"] = "productfile_enabled";
+            $definitions["file_enabled"] = "tinyint(1) NOT NULL DEFAULT '0'";
+        
+        if ($this->changeTableFields( $table, $fields, $definitions, $newnames ))
+        {
+            // Update config to say this has been done already
+            JTable::addIncludePath( JPATH_ADMINISTRATOR.DS.'components'.DS.'com_tienda'.DS.'tables' );
+            $table = JTable::getInstance( 'Config', 'TiendaTable' );
+            $table->load( 'checkProductFiles' );
+            $table->config_name = 'checkProductFiles';
+            $table->value = '1';
+            $table->save();
+            return true;
+        }
+
+        return false;        
+    }
 
 }
