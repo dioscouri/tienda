e9f85bd5280c6523a283566f2f0128d6f04949b6
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2010-03-07 00:11:31 +0000

    Updated all version numbers to 0.3.0 (cannot believe I forgot to do this before tagging... DOH)

--- tienda/admin/defines.php
+++ tienda/admin/defines.php
@@ -12,7 +12,7 @@ defined('_JEXEC') or die('Restricted access');
 
 class Tienda extends JObject
 {
-    static $_version 		= '0.2.1';
+    static $_version 		= '0.3.0';
     static $_versiontype    = 'community';
     static $_copyrightyear 	= '2010';
     static $_name 			= 'tienda';
