3bf1f0065139ef6a8838aee3eadbb5c037e84705
Daniele Rosario <realskullbock@gmail.com>
2010-03-09 13:50:02 +0000

    Added a module to display a quick icon in the joomla admin dashboard.

---
+++ tienda/mod_tienda_quickicon/mod_tienda_quickicon.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @version	1.5
+ * @package	Tienda
+ * @author 	Daniele Rosario
+ * @link 	http://www.dioscouri.com
+ * @copyright Copyright (C) 2010 Daniele Rosario. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+*/
+
+/** ensure this file is being included by a parent file */
+defined('_JEXEC') or die('Restricted access');
+
+$text = $params->get( 'text', 'Tienda Dashboard' );
+
+$mainframe =& JFactory::getApplication();
+$document =& JFactory::getDocument();
+
+if(!class_exists("Tienda"))
+	JLoader::import( 'com_tienda.defines', JPATH_ADMINISTRATOR.DS.'components' );
+
+$img = Tienda::getURL()."images/tienda.png";
+
+require( JModuleHelper::getLayoutPath( 'mod_tienda_quickicon' ) );
---
+++ tienda/mod_tienda_quickicon/tmpl/default.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @version    1.5
+ * @package    Tienda
+ * @author     Daniele Rosario
+ * @link     http://www.dioscouri.com
+ * @copyright Copyright (C) 2010 Daniele Rosario. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+*/
+
+/** ensure this file is being included by a parent file */
+defined('_JEXEC') or die('Restricted access');
+
+$document->addStyleSheet(Tienda::getURL()."/css/tienda_admin.css");
+?>
+<div class="tcpanel">
+<div style="float:<?php echo ($lang->isRTL()) ? 'right' : 'left'; ?>;">
+<div class="icon"><a href="index.php?option=com_tienda">
+<img src="<?php echo $img ?>" />
+<span><?php echo $text; ?></span> </a></div>
+</div>
+</div>
