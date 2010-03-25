1f6ac0ae1899e339adcce3d5a593abb6c3d0c889
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2010-03-25 03:26:53 +0000

    Removed link to edit user address from the user_address admin-side module, fixes #688

--- tienda/mod_tienda_useraddress/tmpl/default.php
+++ tienda/mod_tienda_useraddress/tmpl/default.php
@@ -32,40 +32,38 @@ if ($num > 0 && $addresses)
         ?>
         <tr>
             <td style="text-align: center;">
-                        <a href="<?php echo JRoute::_( $address->link ); ?>">
-                            <?php echo $address->address_name; ?>
-                        </a>
-                    </td>
-                    <td style="text-align: left;">
-                        <?php // TODO Use sprintf to enable formatting?  How best to display addresses? ?>
-                        <!-- ADDRESS -->
-                        <b><?php echo @$address->first_name; ?> <?php echo @$address->middle_name; ?> <?php echo @$address->last_name; ?></b><br/>
-                        <?php if (!empty($address->company)) { echo $address->company; ?><br/><?php } ?>
-                        <?php echo $address->address_1; ?><br/>
-                        <?php if (!empty($address->address_2)) { echo $address->address_2; ?><br/><?php } ?>
-                        <?php echo @$address->city; ?>, <?php echo @$address->zone_name; ?> <?php echo @$address->postal_code; ?><br/>
-                        <?php echo @$address->country_name; ?><br/>
-                        <!-- PHONE NUMBERS -->
-                        <?php // if ($address->phone_1 || $address->phone_2 || $address->fax) { echo "<hr/>"; } ?>
-                        <?php if (!empty($address->phone_1)) { echo "&nbsp;&bull;&nbsp;<b>".JText::_( "Phone" )."</b>: ".$address->phone_1; ?><br/><?php } ?>
-                        <?php if (!empty($address->phone_2)) { echo "&nbsp;&bull;&nbsp;<b>".JText::_( "Alt Phone" )."</b>: ".$address->phone_2; ?><br/><?php } ?>
-                        <?php if (!empty($address->fax)) { echo "&nbsp;&bull;&nbsp;<b>".JText::_( "Fax" )."</b>: ".$address->fax; ?><br/><?php } ?>
-                    </td>
-                    <td style="text-align: center;">
-                        <?php if ($address->is_default_shipping && $address->is_default_billing)
-                        {
-                            echo JText::_( "Default Billing and Shipping Address" );
-                        }
-                        elseif ($address->is_default_shipping) 
-                        {
-                        echo JText::_( "Default Shipping Address" );
-                        }
-                        elseif ($address->is_default_billing) 
-                        {
-                        echo JText::_( "Default Billing Address" );
-                        }
-                        ?>
-                    </td>
+                <?php echo $address->address_name; ?>
+            </td>
+            <td style="text-align: left;">
+                <?php // TODO Use sprintf to enable formatting?  How best to display addresses? ?>
+                <!-- ADDRESS -->
+                <b><?php echo @$address->first_name; ?> <?php echo @$address->middle_name; ?> <?php echo @$address->last_name; ?></b><br/>
+                <?php if (!empty($address->company)) { echo $address->company; ?><br/><?php } ?>
+                <?php echo $address->address_1; ?><br/>
+                <?php if (!empty($address->address_2)) { echo $address->address_2; ?><br/><?php } ?>
+                <?php echo @$address->city; ?>, <?php echo @$address->zone_name; ?> <?php echo @$address->postal_code; ?><br/>
+                <?php echo @$address->country_name; ?><br/>
+                <!-- PHONE NUMBERS -->
+                <?php // if ($address->phone_1 || $address->phone_2 || $address->fax) { echo "<hr/>"; } ?>
+                <?php if (!empty($address->phone_1)) { echo "&nbsp;&bull;&nbsp;<b>".JText::_( "Phone" )."</b>: ".$address->phone_1; ?><br/><?php } ?>
+                <?php if (!empty($address->phone_2)) { echo "&nbsp;&bull;&nbsp;<b>".JText::_( "Alt Phone" )."</b>: ".$address->phone_2; ?><br/><?php } ?>
+                <?php if (!empty($address->fax)) { echo "&nbsp;&bull;&nbsp;<b>".JText::_( "Fax" )."</b>: ".$address->fax; ?><br/><?php } ?>
+            </td>
+            <td style="text-align: center;">
+                <?php if ($address->is_default_shipping && $address->is_default_billing)
+                {
+                    echo JText::_( "Default Billing and Shipping Address" );
+                }
+                elseif ($address->is_default_shipping) 
+                {
+                echo JText::_( "Default Shipping Address" );
+                }
+                elseif ($address->is_default_billing) 
+                {
+                echo JText::_( "Default Billing Address" );
+                }
+                ?>
+            </td>
         </tr>
         <?php
     } 
@@ -76,6 +74,5 @@ if ($num > 0 && $addresses)
 }  
     elseif ($display_null == '1') 
 {
-    $text = JText::_( $null_text );
-    echo $text;
+    echo JText::_( $null_text );
 }
