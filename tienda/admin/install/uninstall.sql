c6f3718090f1c87954afa6a8f1b3245a64d78b8e
jhuntwork <jhuntwork@dioscouri.com>
2010-04-06 16:16:51 +0000

    Tweak to SQL uninstall file - reset original settings once done dropping tables.

--- tienda/admin/install/uninstall.sql
+++ tienda/admin/install/uninstall.sql
@@ -44,3 +44,7 @@ drop table `#__tienda_taxrates`;
 drop table `#__tienda_userinfo`;
 drop table `#__tienda_zonerelations`;
 drop table `#__tienda_zones`;
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
