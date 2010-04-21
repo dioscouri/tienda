ab186526ec4262175b49d1c5df39df277f52bc97
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2010-04-21 01:22:12 +0000

    refs #786: Created frame for categories module

---
+++ tienda/mod_tienda_categories/mod_tienda_categories.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package	Tienda
+ * @author 	Dioscouri Design
+ * @link 	http://www.dioscouri.com
+ * @copyright Copyright (C) 2009 Dioscouri Design. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+*/
+
+/** ensure this file is being included by a parent file */
+defined('_JEXEC') or die('Restricted access');
+
+require_once( dirname(__FILE__).DS.'helper.php' );
+
+// include lang files
+$lang = JFactory::getLanguage();
+$lang->load( 'com_tienda', JPATH_BASE );
+$lang->load( 'com_tienda', JPATH_ADMINISTRATOR );
+
+// grab the items
+$helper = new modTiendaCategoriesHelper( $params ); 
+$items = $helper->getItems();
+
+require( JModuleHelper::getLayoutPath( 'mod_tienda_categories' ) );
---
+++ tienda/mod_tienda_categories/tmpl/default.php
@@ -0,0 +1,3 @@
+<?php defined('_JEXEC') or die('Restricted access');
+$document->addStyleSheet( JURI::root(true).'/modules/mod_tienda_categories/tmpl/tienda_categories.css');
+?>
