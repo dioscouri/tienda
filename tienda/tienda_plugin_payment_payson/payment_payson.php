e467fc5fe96e43a3f5c505643d42ea1e93b2c06f
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2010-05-01 13:27:05 +0000

    Added pagesguro payment plugin to manifest file, refs #1300 Fixed bugs in pagseguro and payson install xml files, refs #1298

--- tienda/tienda_plugin_payment_payson/payment_payson.php
+++ tienda/tienda_plugin_payment_payson/payment_payson.php
@@ -11,7 +11,7 @@
 /** ensure this file is being included by a parent file */
 defined('_JEXEC') or die('Restricted access');
 
-JLoader::import( 'com_tienda.library.plugins.payment', JPATH_ADMINISTRATOR.DS.'components' );
+Tienda::load( 'TiendaPaymentPlugin', 'library.plugins.payment' );
 
 class plgTiendaPayment_payson extends TiendaPaymentPlugin
 {
