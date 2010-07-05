5c78182d318e7442c5fb24f82e78b441dfa040d8
rksingh <rksingh@dioscouri.com>
2010-07-05 04:05:02 +0000

    Made the change in the options of my order module refs #1483

--- tienda/mod_tienda_my_order/tmpl/default.php
+++ tienda/mod_tienda_my_order/tmpl/default.php
@@ -14,29 +14,32 @@ defined('_JEXEC') or die('Restricted access');
 // Add CSS
 $document->addStyleSheet( JURI::root(true).'/modules/mod_tienda_my_order/tmpl/tienda_my_order.css');
 
-$resize = false;
-$options = array();
-if ($params->get('display_image_width', '') != '')
-{
-	$options['width'] = $params->get('display_image_width');
-}
-if ($params->get('display_image_height', '') != '')
-{
-	$options['height'] = $params->get('display_image_height');
-} 
 if ($num > 0 && @$orders)
 { ?>
 	<table style="clear: both;" class="adminlist">
         <thead>
               <tr>
+              <?php if ($params->get('display_id', '1') == '1')
+				{?>
                 <th style="width: 50px;">
                    ID </th>
+                <?php  }?>
+                <?php if ($params->get('display_date', '1') == '1')
+				{?>
                 <th style="width: 200px;">
-                  Date                </th>
+                  Date           
+                 </th>
+                  <?php  }?>
+                  <?php if ($params->get('display_price', '1') == '1')
+				{?>
                 <th style="width: 100px;">
                    Total</th>
+                <?php  }?>
+                 <?php if ($params->get('display_state', '1') == '1')
+				{?>
                 <th style="width: 100px;">
                     State                </th>
+                 <?php  }?>
             </tr>
         </thead> <tbody>
   <?php  // Loop through the products to display
@@ -44,20 +47,29 @@ if ($num > 0 && @$orders)
 	 $count=0;
 	  foreach (@$orders as $order) : ?>
 		 <tr class="row<?php echo $count?> ">
-		 <td>
+		 <?php if ($params->get('display_id', '1') == '1') {?>
+		<td>
 		 <a href="<?php echo $order->link ?>"><?php echo $order ->order_id?></a> 
 		 </td>
+        <?php  }?>
+		 <?php if ($params->get('display_date', '1') == '1') {?>
 		  <td>
 		<a href="<?php echo $order->link; ?>">
-                        <?php echo JHTML::_('date', $order->created_date, TiendaConfig::getInstance()->get('date_format')); ?>
-                    </a>
+          <?php echo JHTML::_('date', $order->created_date, TiendaConfig::getInstance()->get('date_format')); ?>
+         </a>
 		 </td>
+		 <?php } ?>
+		   <?php if ($params->get('display_price', '1') == '1')
+				{?>
 		 <td>
 		<?php echo $order ->order_total?>
 		 </td>
+		   <?php } if ($params->get('display_state', '1') == '1')
+				{?>
 		  <td>
 		 <?php echo $order ->order_state_name?> 
 		 </td>
+		 <?php } ?>
 		 </tr>
 		
   <?php  endforeach; ?>
