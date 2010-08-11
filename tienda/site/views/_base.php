dcf0ac1f54e50a35fd852de3717c57b5a695de2a
djanubis <djanubis@dioscouri.com>
2010-08-11 07:39:03 +0000

    Added JText entries for menu internationalization.

--- tienda/site/views/_base.php
+++ tienda/site/views/_base.php
@@ -13,22 +13,22 @@ defined('_JEXEC') or die('Restricted access');
 
 jimport( 'joomla.application.component.view' );
 
-class TiendaViewBase extends JView 
+class TiendaViewBase extends JView
 {
 	/**
 	 * The valid task set by the controller
 	 * @var str
 	 */
 	protected $_doTask;
-	
+
     /**
      * First displays the submenu, then displays the output
      * but only if a valid _doTask is set in the view object
-     * 
+     *
      * @param $tpl
      * @return unknown_type
      */
-    function display($tpl=null) 
+    function display($tpl=null)
     {
     	// display() will return null if 'doTask' is not set by the controller
     	// This prevents unauthorized access by bypassing the controllers
@@ -36,28 +36,28 @@ class TiendaViewBase extends JView
     	{
     		return null;
     	}
-    	
+
         $this->getLayoutVars($tpl);
-        
+
         Tienda::load( 'TiendaMenu', 'library.menu' );
-        
+
         if (!JRequest::getInt('hidemainmenu') && empty($this->hidemenu))
         {
-            $this->displaySubmenu();	
+            $this->displaySubmenu();
         }
-        
+
         parent::display($tpl);
     }
-	
+
 	/**
 	 * Displays a submenu if there is one and if hidemainmenu is not set
-	 * 
+	 *
 	 * @param $selected
 	 * @return unknown_type
 	 */
 	function displaySubmenu($selected='')
 	{
-		if (!JRequest::getInt('hidemainmenu') && empty($this->hidemenu)) 
+		if (!JRequest::getInt('hidemainmenu') && empty($this->hidemenu))
 		{
 			jimport('joomla.html.toolbar');
 			require_once( JPATH_ADMINISTRATOR.DS.'includes'.DS.'toolbar.php' );
@@ -66,14 +66,14 @@ class TiendaViewBase extends JView
 			$menu =& TiendaMenu::getInstance();
 			if (!empty(JFactory::getUser()->id))
 			{
-			    $menu->addEntry('Dashboard', 'index.php?option=com_tienda&view=dashboard', $view == 'dashboard' ? true : false );
-	            $menu->addEntry('Profile', 'index.php?option=com_tienda&view=accounts', $view == 'accounts' ? true : false );
-                $menu->addEntry('Order History', 'index.php?option=com_tienda&view=orders', $view == 'orders' ? true : false);
+			    $menu->addEntry(JText::_('Dashboard'), 'index.php?option=com_tienda&view=dashboard', $view == 'dashboard' ? true : false );
+	            $menu->addEntry(JText::_('Profile'), 'index.php?option=com_tienda&view=accounts', $view == 'accounts' ? true : false );
+                $menu->addEntry(JText::_('Order History'), 'index.php?option=com_tienda&view=orders', $view == 'orders' ? true : false);
 			}
-			$menu->addEntry('Shopping Cart', 'index.php?option=com_tienda&view=carts', $view == 'carts' ? true : false);
+			$menu->addEntry(JText::_('Shopping Cart'), 'index.php?option=com_tienda&view=carts', $view == 'carts' ? true : false);
 		}
 	}
-	
+
     /**
     * Sets the task to something valid
     *
@@ -91,7 +91,7 @@ class TiendaViewBase extends JView
 
     /**
      * Gets layout vars for the view
-     * 
+     *
      * @return unknown_type
      */
     function getLayoutVars($tpl=null)
@@ -112,10 +112,10 @@ class TiendaViewBase extends JView
               break;
         }
     }
-    
+
 	/**
 	 * Basic commands for displaying a list
-	 * 
+	 *
 	 * @param $tpl
 	 * @return unknown_type
 	 */
@@ -124,16 +124,16 @@ class TiendaViewBase extends JView
 		Tienda::load( 'TiendaSelect', 'library.select' );
 		Tienda::load( 'TiendaGrid', 'library.grid' );
 		$model = $this->getModel();
-		
+
 		// set the model state
 			$this->assign( 'state', $model->getState() );
-			
+
 		// page-navigation
 			$this->assign( 'pagination', $model->getPagination() );
-		
+
 		// list of items
 			$this->assign('items', $model->getList());
-			
+
 		// form
 			$validate = JUtility::getToken();
 			$form = array();
@@ -142,7 +142,7 @@ class TiendaViewBase extends JView
 			$form['validate'] = "<input type='hidden' name='{$validate}' value='1' />";
 			$this->assign( 'form', $form );
 	}
-	
+
 	/**
 	 * Basic methods for a form
 	 * @param $tpl
@@ -152,10 +152,10 @@ class TiendaViewBase extends JView
 	{
 		Tienda::load( 'TiendaSelect', 'library.select' );
 		$model = $this->getModel();
-			
+
 		// get the data
 			$this->assign('row', $model->getItem() );
-		
+
 		// form
 			$form = array();
             $controller = strtolower( $this->get( '_controller', JRequest::getVar('controller', JRequest::getVar('view') ) ) );
@@ -166,7 +166,7 @@ class TiendaViewBase extends JView
 			$form['validate'] = "<input type='hidden' name='".JUtility::getToken()."' value='1' />";
 			$form['id'] = $model->getId();
 			$this->assign( 'form', $form );
-			
+
 		// set the required image
 		// TODO Fix this
 			$required = new stdClass();
@@ -174,5 +174,5 @@ class TiendaViewBase extends JView
 			$required->image = "<img src='".JURI::root()."/media/com_tienda/images/required_16.png' alt='{$required->text}'>";
 			$this->assign('required', $required );
 	}
-	
+
 }
