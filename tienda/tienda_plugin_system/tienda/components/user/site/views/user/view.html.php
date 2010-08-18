06480d3aa27667aefd50d9d03738bbf77e23cf6f
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2010-08-18 15:06:21 +0000

    Added jimport to the user view override to avoid potential errors of jview not already being included

--- tienda/tienda_plugin_system/tienda/components/user/site/views/user/view.html.php
+++ tienda/tienda_plugin_system/tienda/components/user/site/views/user/view.html.php
@@ -15,6 +15,7 @@
 // Check to ensure this file is included in Joomla!
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
+jimport( 'joomla.application.component.view' );
 Tienda::load( 'TiendaViewBase', 'views._base' );
 
 /**
