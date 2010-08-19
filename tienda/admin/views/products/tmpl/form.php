7f73f8da08a21506d9d6ccc51c473fd2e3e8bea6
zurli <zurli@dioscouri.com>
2010-08-19 21:56:20 +0000

    solved Product, Images missing View Gallery translation issue, refs #284

--- tienda/admin/views/products/tmpl/form.php
+++ tienda/admin/views/products/tmpl/form.php
@@ -374,7 +374,7 @@ window.addEvent('domready', function(){
                     <td>
                         [
                         <?php
-                        echo TiendaUrl::popup( "index.php?option=com_tienda&view=products&task=viewGallery&id=".@$row->product_id."&tmpl=component", "View Gallery" ); 
+                        echo TiendaUrl::popup( "index.php?option=com_tienda&view=products&task=viewGallery&id=".@$row->product_id."&tmpl=component", JText::_( "View Gallery" ) ); 
                         ?>
                         ]
                         <br/>
