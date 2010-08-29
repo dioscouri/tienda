5f28d9041dec7ecae4894778439ba3c29b53a640
zurli <zurli@dioscouri.com>
2010-08-29 08:33:23 +0000

    database translation to german, refs #2560

---
+++ language_packages/de-DE/SQL/defaultdata-ordersates.sql
@@ -0,0 +1,19 @@
+-- -----------------------------------------------------
+-- Dumping data for table `#__tienda_orderstates`
+-- -----------------------------------------------------
+INSERT IGNORE INTO `#__tienda_orderstates` (`order_state_id`, `order_state_name`) VALUES
+(1, 'Offen'),
+(2, 'In Bearbeitung'),
+(3, 'Versandt'),
+(5, 'Abgeschlossen'),
+(7, 'Annuliert'),
+(8, 'Abgelehnt'),
+(9, 'Storno abgebrochen'),
+(10, 'Fehlgeschlagen'),
+(11, 'Zurückerstattet'),
+(12, 'Storniert'),
+(13, 'Antrag Rückerstattung'),
+(14, 'Unbekannter Fehler'),
+(15, 'Vorauszahlung'),
+(16, 'Zahlung geplant'),
+(17, 'Zahlung eingegangen');
