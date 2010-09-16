3c52b73000dea7e3de3a2e7b438915c626a38c61
rksingh <rksingh@dioscouri.com>
2010-09-16 12:37:49 +0000

    Applied the CSS on the Advance Search refs #3096 and refs #2805

--- tienda/site/views/search/tmpl/default.php
+++ tienda/site/views/search/tmpl/default.php
@@ -3,22 +3,32 @@
 <?php $state = @$this->state; ?>
 <?php $form = @$this->form; ?>
 <?php $items = @$this->items; ?>
-
+<style type="text/css">
+.filtergroup .label{width:200px; display:block; float:left; padding-top:3px;}
+.filtergroup .row{float:left:width:100%; padding:5px 0;}
+.filtergroup .submit{padding-left:200px;}
+.filtergroup .Price .field, .filtergroup .Price .field div{float:left;}
+.filtergroup .Price .field .label{width:auto; padding-right:5px;}
+.filtergroup .Price .field div{padding-right:20px;}
+.filtergroup .clear{clear:both;}
+.filtergroup input.text{width:250px;}
+.filtergroup select.inputbox, .filtergroup .filtershipping select{width:256px;}
+.pager{float:left; width:100%; height:30px;}
+</style>
 <form action="<?php echo JRoute::_( @$form['action'] )?>" method="post" name="adminForm" enctype="multipart/form-data">
 
 	<?php echo TiendaGrid::pagetooltip( JRequest::getVar('view') ); ?>
-    <table>
-      <tr class="filterline">
-                <td>
+    <div class="filtergroup" >
+    <h2> <?php echo JText::_("Advanced Search"); ?></h2>
                 	<?php $attribs = array('class' => 'inputbox', 'size' => '1', 'onchange' => 'document.adminForm.submit();'); ?>
                    
-                    <div class="filtername" >
+                    <div class="row filtername" >
                     <span class="label"><?php echo JText::_("Name"); ?>:</span> 
-                    <input id="filter_name" name="filter_name" value="<?php echo @$state->filter_name; ?>" size="25"/>
+                    <input id="filter_name" name="filter_name" class="text" value="<?php echo @$state->filter_name; ?>" size="25"/>
                     </div>            
                 	
                 	
-                	<div class="filtermulticategory" >
+                	<div class="row filtermulticategory" >
                     <span class="label"><?php echo JText::_("Category"); ?>: </span> 
                     <?php $catattribut=array('class' => 'inputbox', 'size' => '1','multiple' => 'yes' , 'size'=>3 );?>
                 	<?php echo TiendaSelect::category( @$state->filter_multicategory, 'filter_multicategory[]',$catattribut , 'filter_multicategory', true ); ?>
@@ -26,60 +36,61 @@
                 	
                 	
                 	
-                	<div class="filtershipping" >
+                	<div class="row filtershipping" >
                         <span class="label"><?php echo JText::_("Requires Shipping"); ?>: </span> 
                         <?php echo TiendaSelect::booleans( @$state->filter_ships, 'filter_ships', '', 'ships', true, "Doesn't Matter", 'Yes', 'No' ); ?>
                 	 </div>
                 	
-                	 <div class="filtersku" >   
+                	 <div class="row filtersku" >   
                 	  <span class="label"><?php echo JText::_("SKU"); ?>: </span>         	                
-                	<input id="filter_sku" name="filter_sku" value="<?php echo @$state->filter_sku; ?>" size="15"/>
+                	<input id="filter_sku" name="filter_sku" class="text" value="<?php echo @$state->filter_sku; ?>" size="15"/>
                     </div>
                     
-                	<div class="Price">
+                	<div class="row Price">
 	                   	<div class="rangeline">
 	                	<span class="label"><?php echo JText::_("Price"); ?>: </span> 
-	                		<span class="label"><?php echo JText::_("From"); ?>:</span> <input id="filter_price_from" name="filter_price_from" value="<?php echo @$state->filter_price_from; ?>" size="5" class="input" />
-	                		<span class="label"><?php echo JText::_("To"); ?>:</span> <input id="filter_price_to" name="filter_price_to" value="<?php echo @$state->filter_price_to; ?>" size="5" class="input" />
-	                	</div>
+	                	<div class="field">
+	                		<div><span class="label"><?php echo JText::_("From"); ?>:</span> <input id="filter_price_from" name="filter_price_from" value="<?php echo @$state->filter_price_from; ?>" size="5" class="input" /></div>
+	                		<div><span class="label"><?php echo JText::_("To"); ?>:</span> <input id="filter_price_to" name="filter_price_to" value="<?php echo @$state->filter_price_to; ?>" size="5" class="input" /></div>
+	                	</div></div>
                 	</div>
+                	
+                	<div class="clear"></div>
                 
-                	<div class="quantity">
+                	<div class="row quantity">
 	                	<span class="label"><?php echo JText::_("Show only Items that are in Stock"); ?>: </span>
 	                	<?php echo TiendaSelect::booleanlist( 'filter_stock', '', @$state->filter_stock ); ?> 
                 	</div>
                 	
-                	<div class="filterdescription" >   
+                	<div class="row filterdescription" >   
                 	  <span class="label"><?php echo JText::_("Description"); ?>: </span>         	                
-                	<input id="filter_description" name="filter_description" value="<?php echo @$state->filter_description; ?>" size="15"/>
+                	<input id="filter_description" name="filter_description" class="text" value="<?php echo @$state->filter_description; ?>" size="15"/>
                     </div>
                     
-                    <div class="filtermanufacturer" >   
+                    <div class="row filtermanufacturer" >   
                 	  <span class="label"><?php echo JText::_("Manufacturer"); ?>: </span>         	                
-                	<input id="filter_manufacturer" name="filter_manufacturer" value="<?php echo @$state->filter_manufacturer; ?>" size="15"/>
+                	<input id="filter_manufacturer" name="filter_manufacturer" class="text" value="<?php echo @$state->filter_manufacturer; ?>" size="15"/>
                     </div>
                     
-                    <div class="submint" >   
+                    <div class="row submit" >   
                 	 <input id="filter_submit" name="filter_submit" type="submit" value="<?php echo JText::_("Search") ?>"/>
+                	  <input id="filter_submit" name="filter_submit" type="submit" value="<?php echo JText::_("Search") ?>"/>
                     </div>
-               
-                </td>
-            </tr>
-    </table>
+            </div>
 	<hr width="100%"> 
-	<table class="adminlist" style="clear: both;">
+	<table class="adminlist" style="clear: both; width: 100% ;">
 		<thead>
             <tr>
-                <th style="width: 5px;">
+                <th style="width: 10%;">
                 	<?php echo JText::_("Num"); ?>
                 </th>
-                 <th style="text-align: left;" colspan="2">
+                 <th style="text-align: left; width: 50%;" colspan="2">
                 	<?php echo TiendaGrid::sort( 'Name', "tbl.product_name", @$state->direction, @$state->order ); ?>
                 </th>
-                <th style="width: 70px;">
+                <th style="width: 20%;">
                 	<?php echo TiendaGrid::sort( 'SKU', "tbl.product_sku", @$state->direction, @$state->order ); ?>
                 </th>
-                <th style="width: 50px;">
+                <th style="width: 20%;">
                 	<?php echo TiendaGrid::sort( 'Price', "price", @$state->direction, @$state->order ); ?>
                 </th>
              </tr>
@@ -97,10 +108,10 @@
 		<?php $i=0; $k=0; ?>
         <?php foreach (@$items as $item) : ?>
             <tr class='row<?php echo $k; ?>'>
-				<td align="center">
+				<td align="left">
 					<?php echo $i + 1; ?>
 				</td>
-				<td style="text-align: center; width: 50px;">
+				<td style="text-align: left; width: 50px;">
                     <?php echo TiendaHelperProduct::getImage($item->product_id, 'id', $item->product_name, 'full', false, false, array( 'width'=>48 ) ); ?>
 				</td> 
 				<td style="text-align: left;">
@@ -108,10 +119,10 @@
 						<?php echo JText::_($item->product_name); ?>
 					</a>
 				</td>
-				<td style="text-align: center;">
+				<td style="text-align: left">
 					<?php echo $item->product_sku; ?>
 				</td>
-				<td style="text-align: right;">
+				<td style="text-align: left">
 					<?php echo TiendaHelperBase::currency($item->price); ?>
 				</td>
 				
@@ -130,13 +141,11 @@
 		
 	</table>
 	<hr width="100%"> 
-	<table>
-	<tr>
-				<th colspan="20" style="font-weight: normal;">
-					<div style="float: right; padding: 5px;"><?php echo @$this->pagination->getResultsCounter(); ?></div>
+	<div class="pager">
+					<div style="float: right;"><?php echo @$this->pagination->getResultsCounter(); ?></div>
 					<div style="float: left;"><?php echo @$this->pagination->getListFooter(); ?></div>
-				</th>
-			</tr></table>
+				
+	</div>
 
 	<input type="hidden" name="order_change" value="0" />
 	<input type="hidden" name="id" value="" />
