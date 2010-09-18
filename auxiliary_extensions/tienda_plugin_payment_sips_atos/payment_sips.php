1ef09c1e16c3d7016de6ea82373cdfbaee453a79
alatak <alatak@dioscouri.com>
2010-09-18 14:10:56 +0000

    refs #2765 : Adding Subversion Keyword Substitution  (trying another syntax)

--- auxiliary_extensions/tienda_plugin_payment_sips_atos/payment_sips.php
+++ auxiliary_extensions/tienda_plugin_payment_sips_atos/payment_sips.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
- * @Id          $Id
- * @Revision    $revision
- * @Date        $Date
+ * @Id          $Id$
+ * @Revision    $Revision$
+ * @Date        $Date$
  * @version	1.5
  * @package	Tienda
- * @author 	$Author
+ * @author 	$Author$
  * @link 	http://www.alatak.net
  * @copyright Copyright (C) 2007 Dioscouri Design. All rights reserved.
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
