0eae41ca4b4a72e227f01e291a4d6ff113309760
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2010-10-01 16:11:12 +0000

    Changed revision number for 0.5.6

--- tienda/admin/defines.php
+++ tienda/admin/defines.php
@@ -13,7 +13,7 @@ defined('_JEXEC') or die('Restricted access');
 class Tienda extends JObject
 {
     static $_version 		= '0.5.6';
-    static $_build          = 'r684';
+    static $_build          = 'r889';
     static $_versiontype    = 'community';
     static $_copyrightyear 	= '2010';
     static $_name 			= 'tienda';
