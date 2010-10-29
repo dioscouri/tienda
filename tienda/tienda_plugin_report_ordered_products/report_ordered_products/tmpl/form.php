fef8d4ffbd12e69da9f73e61dd3f0a4f0bc2eb1a
b.nisevic <b.nisevic@dioscouri.com>
2010-10-29 16:24:07 +0000

    Report on Ordered Products, update,ref #3510

--- tienda/tienda_plugin_report_ordered_products/report_ordered_products/tmpl/form.php
+++ tienda/tienda_plugin_report_ordered_products/report_ordered_products/tmpl/form.php
@@ -9,7 +9,7 @@
     // TODO Convert this to just a text input box so the admin can filter by product_name 
     ?>
 	<?php echo JText::_("Select Product"); ?>:
-	<?php echo TiendaSelect::product(@$state->filter_productid ? $state->filter_productid : 'product_name', 'product_name', $attribs, $idtag, $allowAny, $allowNone, $title = 'Select Product', $title_none = 'No Parent', $enabled = null ); ?>
+	<input type="text" name="product_name" id="product_name" value="<?php echo @$state->product_name; ?>" style="width: 250px;"/>
 	<br/><br/>
 	<?php echo JText::_("Select Date Range"); ?>:	
 	<?php $attribs = array('class' => 'inputbox', 'size' => '1'); ?>	
@@ -18,5 +18,4 @@
 	<?php echo JHTML::calendar( @$state->filter_date_from, "filter_date_from", "filter_date_from", '%Y-%m-%d %H:%M:%S' ); ?>
 	<span class="label"><?php echo JText::_("To"); ?>:</span>
 	<?php echo JHTML::calendar( @$state->filter_date_to, "filter_date_to", "filter_date_to", '%Y-%m-%d %H:%M:%S' ); ?>
-	<input type="hidden" name="filter_datetype" value="created" />
 </div>
