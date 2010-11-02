9f614cf11a63b80c155bbf60ddf038cead5d0115
geomaneo <geomaneo@dioscouri.com>
2010-11-02 12:34:33 +0000

    Fix the issue that the ratings/star still shows in the front end even if the configurations Product Reviews is disabled. refs #3332

--- tienda/site/views/products/tmpl/default.php
+++ tienda/site/views/products/tmpl/default.php
@@ -92,14 +92,14 @@ $citems = @$this->citems;
                             </a>
                         </span>
                     </div>
-                    
+                     <?php if ( TiendaConfig::getInstance()->get('product_review_enable', '0') ) { ?>
                     <div class="product_rating">
                        <?php echo TiendaHelperProduct::getRatingImage( $item->product_rating ); ?>
                        <?php if (!empty($item->product_comments)) : ?>
                        <span class="product_comments_count">(<?php echo $item->product_comments; ?>)</span>
                        <?php endif; ?>
                     </div>
-                    
+                    <?php } ?>
                     <?php if (!empty($item->product_model) || !empty($item->product_sku)) { ?>
                         <div class="product_numbers">
                             <span class="model">
