778469c817f6ec0de441a4773f7ef62e8032094c
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2010-12-08 03:33:47 +0000

    Changed footer to Tienda Ecommerce

--- tienda/site/views/dashboard/tmpl/footer.php
+++ tienda/site/views/dashboard/tmpl/footer.php
@@ -8,6 +8,6 @@ if ($amigosid = TiendaConfig::getInstance()->get( 'amigosid', '' ))
 ?>
 
 <p align="center">
-	<?php echo JText::_( 'Powered by' )." <a href='{$url}' target='_blank'>".JText::_( 'Tienda' )."</a>"; ?>
+	<?php echo JText::_( 'Powered by' )." <a href='{$url}' target='_blank'>".JText::_( 'Tienda Ecommerce' )."</a>"; ?>
 </p>
 
