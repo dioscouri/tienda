c224ee27e12e9d1b2ae324ed5918107c91f5c84f
geomaneo <geomaneo@dioscouri.com>
2010-12-17 16:02:13 +0000

    finished coding. Need to test in livesite refs #3161

--- tienda/site/sef_ext/com_tienda.php
+++ tienda/site/sef_ext/com_tienda.php
@@ -25,7 +25,22 @@ if ($dosef == false) return;
 #---------------=  standard plugin initialize function - don't change =---------------#
 ######################################################################################
 
-//remove common URL from GET vars list, so that they don't show up as query string in the URL
+//load our own lang iso since we dont user sh404sef translation
+$langJoomla =& JFactory::getLanguage();
+$langJoomla->load('com_tienda');
+$langXplode = explode("-",$langJoomla->getTag()); 
+$shLangIso = !empty($langXplode[0]) ? $langXplode[0] : "en";
+
+//load tienda config for sh404sef
+// Check the registry to see if our Tienda class has been overridden
+if ( !class_exists('Tienda') ) 
+    JLoader::register( "Tienda", JPATH_ADMINISTRATOR.DS."components".DS."com_tienda".DS."defines.php" );
+
+// load the config class
+Tienda::load( 'TiendaConfig', 'defines' );
+$tiendaConfig = TiendaConfig::getInstance();
+
+// remove common URL from GET vars list, so that they don't show up as query string in the URL
 shRemoveFromGETVarsList('option');
 shRemoveFromGETVarsList('view');
 shRemoveFromGETVarsList('lang');
@@ -36,23 +51,203 @@ shRemoveFromGETVarsList('limit');
 if (isset($limitstart))
   shRemoveFromGETVarsList('limitstart'); // limitstart can be zero
 
+if (!function_exists( 'shSefGetTiendaProductName')) {
+	function shSefGetTiendaProductName($id, $option, $shLangName, $shLangIso, $tiendaConfig) {
+		if (empty($id)) return null;
+    
+    	$sefConfig = & shRouter::shGetConfig();    	   	
+    	// get DB
+    	static $result = null;
+    	
+    	if(is_null($result)) {
+	    	$database =& JFactory::getDBO();
+	    	$query = "SELECT `product_id`, `product_sku`, `product_alias` FROM `#__tienda_products`";
+	   	 	$query .= "\n WHERE `product_id` = ".$database->Quote( $id);
+	    	$database->setQuery( $query );
+	
+	    	if (!shTranslateUrl($option, $shLangName)) {
+		    	$result = $database->loadObject( false );
+		    }else{
+		    	$result = $database->loadObject();
+		    }
+    	}
+    	
+		//product not available
+	    if(empty($result)) return JText::_('product').$sefConfig->replacement.$id;
+	          	
+	    $shName = '';
+	    if ( $tiendaConfig->get('insert_product_id') ) {
+	    	$shName .= $result->product_id;
+	    }
+    
+	    if ( $tiendaConfig->get('insert_product_sku') ) {
+	    	$shName .= (empty($shName) ? '':$sefConfig->replacement).$result->product_sku;
+	    }
+    
+	    if ($tiendaConfig->get('insert_product_name') ) {
+	    	 $shName .= (empty($shName) ? '':$sefConfig->replacement).$row->product_alias;	    	 
+	    	 if (empty($shName))  $shName = $row->product_name;	    	
+	    }    
+
+    	return $shName;
+	}
+}
+
+if (!function_exists( 'shSefGetTiendaCategoryName')) {
+	function shSefGetTiendaCategoryName($filter_category, $option, $shLangName, $tiendaConfig) {		
+		if(empty($filter_category)) return null;
+		static $cat = null;
+		$shName = '';
+	    $sefConfig = & shRouter::shGetConfig();
+	    if (is_null( $cat)) {
+	      	$database =& JFactory::getDBO();
+	      
+	      	$query = "SELECT `category_id`, `category_name` FROM #__tienda_categories";
+	       	$query .= "\n WHERE `category_id` = ".$database->Quote( $filter_category );
+	      	$database->setQuery( $query);
+	      	if (!shTranslateUrl($option, $shLangName)) {
+	       		$cat = $database->loadObject(false);
+	      	} else {
+	        	$cat = $database->loadObject();
+	      	}         		    
+	    }
+	    
+		$tiendaConfigCat = $tiendaConfig->get('insert_categories');
+	    $tiendaConfigCatInsertid =$tiendaConfig->get('insert_category_id');
+
+	    if($tiendaConfigCatInsertid) {
+	    	$shName .= $cat->category_id;
+	    }
+		   
+	    if($tiendaConfigCat && !empty($cat)) {	    	    	
+	    	$shName .= (empty($shName) ? '':$sefConfig->replacement).$cat->category_name;	    	
+	    }
+	    //TODO: need to add category name of parent categories
+	    	       
+	    return $shName;    	
+	}	
+}
+
+if (!function_exists( 'shSefGetTiendaManufactureryName')) {
+	function shSefGetTiendaManufactureryName($filter_manufaturer, $option, $shLangName, $tiendaConfig) {
+		if(empty($filter_manufaturer)) return null;
+		static $manufacturer = null;
+		$shName = '';
+	    $sefConfig = & shRouter::shGetConfig();
+	    if (is_null( $manufacturer )) {
+	      	$database =& JFactory::getDBO();
+	      
+	      	$query = "SELECT `manufacturer_id`, `manufacturer_name` FROM #__tienda_manufacturers";
+	       	$query .= "\n WHERE `manufacturer_id` = ".$database->Quote( $filter_manufaturer );
+	      	$database->setQuery( $query);
+	      	if (!shTranslateUrl($option, $shLangName)) {
+	       		$manufacturer = $database->loadObject(false);
+	      	} else {
+	        	$manufacturer = $database->loadObject();
+	      	}         		    
+	    }
+	    
+		$tiendaConfigM = $tiendaConfig->get('insert_manufacturer_name');
+	    $tiendaConfigMInsertid =$tiendaConfig->get('insert_manufacturer_id');
+
+	    if($tiendaConfigMInsertid) {
+	    	$shName .= $manufacturer->manufacturer_id;
+	    }
+		   
+	    if($tiendaConfigM && !empty($manufacturer)) {	    	    	
+	    	$shName .= (empty($shName) ? '':$sefConfig->replacement).$manufacturer->manufacturer_name;	    	
+	    }
+	    	    	       
+	    return $shName;  
+	}	
+}
 
 $task = isset($task) ? @$task : null;
 $Itemid = isset($Itemid) ? @$Itemid : null;
-$shName = shGetComponentPrefix($option);
-$shName = empty($shSampleName) ?
-		getMenuTitle($option, $task, $Itemid, null, $shLangName) : $shName;
-//$shName = (empty($shName) || $shName == '/') ? 'calendar':$shName;
-$title[] = $shName;
+$shTiendaName = shGetComponentPrefix($option);
+$shTiendaName = empty($shTiendaName) ?
+		getMenuTitle($option, $task, $Itemid, null, $shTiendaName) : $shTiendaName;
+$shTiendaName = (empty($shTiendaName) || $shTiendaName == '/') ? 'SHOP':$shTiendaName;
+
+if($tiendaConfig->get('insert_shop_name')) {
+	$shopname = $tiendaConfig->get('shop_name');
+	$shopname = ucwords(strtolower($shopname));
+	$title[] = str_replace(" ", "", $shopname);
+}
+
+switch($view):
+	case 'products':						
+		//multiple product inside the category
+		if(!empty($filter_category)) {
+			if($tiendaConfig->get('insert_categories')) {
+				$title[] = shSefGetTiendaCategoryName($filter_category, $option, $shLangName, $tiendaConfig);
+			}
+			shRemoveFromGETVarsList('filter_category');
+		} elseif(!empty($filter_manufacturer)){
+			if($tiendaConfig->get('insert_manufacturer_name')) {
+				$title[] = shSefGetTiendaManufactureryName($filter_category, $option, $shLangName, $tiendaConfig);
+			}
+			shRemoveFromGETVarsList('filter_manufacturer');
+		} else {
+			//single product
+			if(!empty($id)) $title[] = shSefGetTiendaProductName( $id, $option, $shLangName, $shLangIso, $tiendaConfig);
+		}		
+		break;
+	case 'manufacturers':			
+		if(!empty($filter_manufacturer)){
+			if($tiendaConfig->get('insert_manufacturer_name')) {
+				$title[] = shSefGetTiendaManufactureryName($filter_category, $option, $shLangName, $tiendaConfig);
+			}
+		}
+		shRemoveFromGETVarsList('filter_manufacturer');
+		break;
+	case 'dashboard':		
+		$title[] = JText::_('dashboard');		
+		break;	
+	case 'accounts':		
+		$title[] = JText::_('accounts');		
+		break;
+	case 'orders':		
+		$title[] = JText::_('orders');		
+		break;
+	case 'subscriptions':		
+		$title[] = JText::_('subscriptions');		
+		break;
+	case 'carts':		
+		$title[] = JText::_('carts');		
+		break;
+	case 'check':		
+		$title[] = JText::_('carts');		
+		break;
+	case 'addresses':		
+		$title[] = JText::_('addresses');		
+		break;
+	default:
+		$dosef = false;
+		break;
+endswitch;
+
+if(!empty($task)) {
+  	shRemoveFromGETVarsList('task');
+}
+if(!empty($id)) {
+  	shRemoveFromGETVarsList('id');
+}
+if(isset($Itemid)) {
+  	shRemoveFromGETVarsList('Itemid');
+}
+if(!empty($layout)) {
+	shRemoveFromGETVarsList('layout');
+}
 
 ######################################################################################
 #---------------= standard plugin finalize function - don't change =---------------#
-if ($dosef){
-   $string = shFinalizePlugin( $string, $title, $shAppendString, $shItemidString,
-      (isset($limit) ? @$limit : null), (isset($limitstart) ? @$limitstart : null),
+if ($dosef)
+{
+   $string = shFinalizePlugin( $string, $title, $shAppendString, $shItemidString, 
+      (isset($limit) ? @$limit : null), (isset($limitstart) ? @$limitstart : null), 
       (isset($shLangName) ? @$shLangName : null));
-}
+}  
 #---------------=  standard plugin finalize function - don't change =---------------#
 ######################################################################################
-
 ?>
