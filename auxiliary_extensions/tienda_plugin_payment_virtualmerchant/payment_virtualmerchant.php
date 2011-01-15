5bce60370122e74d18b8194bfc26c854eaaad9cd
Skullbock <Skullbock@dioscouri.com>
2011-01-15 17:06:03 +0000

    Frame for VirtualMerchant integration Refs #4097

---
+++ auxiliary_extensions/tienda_plugin_payment_virtualmerchant/payment_virtualmerchant.php
@@ -0,0 +1,506 @@
+<?php
+/**
+ * @version 1.5
+ * @package Tienda
+ * @author  Dioscouri
+ * @link    http://www.dioscouri.com
+ * @copyright Copyright (C) 2011 Dioscouri Design. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+*/
+
+/** ensure this file is being included by a parent file */
+defined('_JEXEC') or die('Restricted access');
+
+Tienda::load( 'TiendaPaymentPlugin', 'library.plugins.payment' );
+
+class plgTiendaPayment_virtualmerchant extends TiendaPaymentPlugin
+{
+    /**
+     * @var $_element  string  Should always correspond with the plugin's filename,
+     *                         forcing it to be unique
+     */
+    var $_element    = 'payment_virtualmerchant';
+
+    /**
+     * Constructor
+     *
+     * For php4 compatability we must not use the __constructor as a constructor for plugins
+     * because func_get_args ( void ) returns a copy of all passed arguments NOT references.
+     * This causes problems with cross-referencing necessary for the observer design pattern.
+     *
+     * @param object $subject The object to observe
+     * @param   array  $config  An array that holds the plugin configuration
+     * @since 1.5
+     */
+    function plgTiendaPayment_virtualmerchant(& $subject, $config) {
+        parent::__construct($subject, $config);
+        $this->loadLanguage( '', JPATH_ADMINISTRATOR );
+    }
+
+    /************************************
+     * Note to 3pd:
+     *
+     * The methods between here
+     * and the next comment block are
+     * yours to modify
+     *
+     ************************************/
+
+
+    /**
+     * Prepares the payment form
+     * and returns HTML Form to be displayed to the user
+     * generally will have a message saying, 'confirm entries, then click complete order'
+     *
+     * Submit button target for onsite payments & return URL for offsite payments should be:
+     * index.php?option=com_tienda&view=checkout&task=confirmPayment&orderpayment_type=xxxxxx
+     * where xxxxxxx = $_element = the plugin's filename
+     *
+     * @param $data     array       form post data
+     * @return string   HTML to display
+     */
+    function _prePayment( $data )
+    {
+        // prepare the payment form
+
+        $vars = new JObject();
+        $vars->order_id = $data['order_id'];
+        $vars->orderpayment_id = $data['orderpayment_id'];
+        $vars->orderpayment_amount = $data['orderpayment_amount'];
+        $vars->orderpayment_type = $this->_element;
+
+        // set payment plugin variables
+        // unecessary for pagseguro
+        /*$vars->merchant_email = $this->_getParam( 'merchant_email' );
+        $vars->post_url = $this->_getPostUrl();
+        $vars->return_url = JURI::root()."index.php?option=com_tienda&view=checkout&task=confirmPayment&orderpayment_type=".$this->_element."&paction=display_message";
+        $vars->cancel_url = JURI::root()."index.php?option=com_tienda&view=checkout&task=confirmPayment&orderpayment_type=".$this->_element."&paction=cancel";
+        $vars->notify_url = JURI::root()."index.php?option=com_tienda&view=checkout&task=confirmPayment&orderpayment_type=".$this->_element."&paction=process&tmpl=component";*/
+        //$vars->currency_code = $this->_getParam( 'currency', 'USD' ); // TODO Eventually use: TiendaConfig::getInstance()->get('currency');
+
+        // set variables for user info
+        $vars->first_name   = $data['orderinfo']->shipping_first_name;
+        $vars->last_name    = $data['orderinfo']->shipping_last_name;
+        $vars->email        = $data['orderinfo']->user_email;
+        $vars->address_1    = $data['orderinfo']->shipping_address_1;
+        $vars->address_2    = $data['orderinfo']->shipping_address_2;
+        $vars->city         = $data['orderinfo']->shipping_city;
+        $vars->country      = $data['orderinfo']->shipping_country_name;
+        $vars->region       = $data['orderinfo']->shipping_zone_name;
+        $vars->postal_code  = $data['orderinfo']->shipping_postal_code;
+
+        $html = $this->_getLayout('prepayment', $vars);
+        return $html;
+    }
+
+    /**
+     * Processes the payment form
+     * and returns HTML to be displayed to the user
+     * generally with a success/failed message
+     *
+     * @param $data     array       form post data
+     * @return string   HTML to display
+     */
+    function _postPayment( $data )
+    {
+        // Process the payment
+        $paction = JRequest::getVar('paction');
+
+        $vars = new JObject();
+
+        switch ($paction)
+        {
+            case "display_message":
+                $vars->message = JText::_('PAYPAL MESSAGE PAYMENT ACCEPTED FOR VALIDATION');
+                $html = $this->_getLayout('message', $vars);
+                $html .= $this->_displayArticle();
+              break;
+            case "process":
+                $vars->message = $this->_process();
+                $html = $this->_getLayout('message', $vars);
+                echo $html; // TODO Remove this
+                $app =& JFactory::getApplication();
+                $app->close();
+              break;
+            case "cancel":
+                $vars->message = JText::_( 'Paypal Message Cancel' );
+                $html = $this->_getLayout('message', $vars);
+              break;
+            default:
+                $vars->message = JText::_( 'Paypal Message Invalid Action' );
+                $html = $this->_getLayout('message', $vars);
+              break;
+        }
+
+        return $html;
+    }
+
+    /**
+     * Prepares variables for the payment form
+     *
+     * @return unknown_type
+     */
+    function _renderForm( $data )
+    {
+        $user = JFactory::getUser();
+        $vars = new JObject();
+
+        $html = $this->_getLayout('form', $vars);
+
+        return $html;
+    }
+
+    /************************************
+     * Note to 3pd:
+     *
+     * The methods between here
+     * and the next comment block are
+     * specific to this payment plugin
+     *
+     ************************************/
+
+    /**
+     * Gets the PagSeguro gateway URL
+     *
+     * @param boolean $full
+     * @return string
+     * @access protected
+     */
+    function _getPostUrl($full = true)
+    {
+        $url = $this->params->get('sandbox') ? 'www.sandbox.paypal.com' : 'www.paypal.com';
+
+        if ($full)
+        {
+            $url = 'https://' . $url . '/cgi-bin/webscr';
+        }
+
+        return $url;
+    }
+
+
+    /**
+     * Gets the value for the Paypal variable
+     *
+     * @param string $name
+     * @return string
+     * @access protected
+     */
+    function _getParam( $name, $default='' )
+    {
+        $return = $this->params->get($name, $default);
+
+        $sandbox_param = "sandbox_$name";
+        $sb_value = $this->params->get($sandbox_param);
+        if ($this->params->get('sandbox') && !empty($sb_value))
+        {
+            $return = $this->params->get($sandbox_param, $default);
+        }
+
+        return $return;
+    }
+
+    /**
+     * Validates the IPN data
+     *
+     * @param array $data
+     * @return string Empty string if data is valid and an error message otherwise
+     * @access protected
+     */
+    function _validateIPN( $data )
+    {
+        $secure_post = $this->params->get( 'secure_post', '0' );
+        $paypal_url = $this->_getPostUrl(false);
+
+        $req = 'cmd=_notify-validate';
+        foreach ($data as $key => $value) {
+            if ($key != 'view' && $key != 'layout') {
+                $value = urlencode($value);
+                $req .= "&$key=$value";
+            }
+        }
+
+        // post back to PayPal system to validate
+        $header  = "POST /cgi-bin/webscr HTTP/1.0\r\n";
+        //$header .= "Host: " . $this->_getPostURL(false) . ":443\r\n";
+        $header .= "Content-Type: application/x-www-form-urlencoded\r\n";
+        $header .= "Content-Length: " . strlen($req) . "\r\n\r\n";
+
+        if ($secure_post) {
+            // If possible, securely post back to paypal using HTTPS
+            // Your PHP server will need to be SSL enabled
+            $fp = fsockopen ('ssl://' . $paypal_url , 443, $errno, $errstr, 30);
+        }
+        else {
+            $fp = fsockopen ($paypal_url, 80, $errno, $errstr, 30);
+        }
+
+        if ( ! $fp) {
+            return JText::sprintf('PAYPAL ERROR POSTING IPN DATA BACK', $errstr, $errno);
+        }
+        else {
+            fputs ($fp, $header . $req);
+            while ( ! feof($fp)) {
+                $res = fgets ($fp, 1024); //echo $res;
+                if (strcmp ($res, 'VERIFIED') == 0) {
+                    return '';
+                }
+                elseif (strcmp ($res, 'INVALID') == 0) {
+                    return JText::_('PAYPAL ERROR IPN VALIDATION');
+                }
+            }
+        }
+
+        fclose($fp);
+        return '';
+    }
+
+    /**
+     *
+     * @return HTML
+     */
+    function _process()
+    {
+        $data = JRequest::get('post');
+
+        // validate the IPN info
+        $error = $this->_validateIPN($data);
+        if (!empty($error))
+        {
+            // ipn Validation failed
+            $data['ipn_validation_results'] = $error;
+        }
+
+        // prepare some data
+        $order_id           = (int) @$data['item_number_1'];
+        $orderpayment_id    = (int) @$data['custom'];
+        $data['transaction_details'] = $this->_getFormattedTransactionDetails( $data );
+
+        // process the payment based on its type
+        if ( !empty($data['txn_type']) )
+        {
+            $payment_error = '';
+
+            if ($data['txn_type'] == 'cart') {
+                // Payment received for multiple items; source is Express Checkout or the PayPal Shopping Cart.
+                $payment_error = $this->_processSale( $data, $error );
+            }
+            elseif (strpos($data['txn_type'], 'subscr_') === 0) {
+                // TODO Complete this
+                //$payment_error = $this->_processSubscription( $data, $error );
+                $payment_error = JText::_( "PAYPAL ERROR INVALID TRANSACTION TYPE SUBSCRIPTIONS UNSUPPORTED" ).": ".$data['txn_type'];
+            }
+            else {
+                // other methods not supported right now
+                $payment_error = JText::_( "PAYPAL ERROR INVALID TRANSACTION TYPE" ).": ".$data['txn_type'];
+            }
+
+            if ($payment_error) {
+                // it seems like an error has occurred during the payment process
+                $error .= $error ? "\n" . $payment_error : $payment_error;
+            }
+        }
+
+        if ($error) {
+            // send an emails to site's administrators with error messages
+            $this->_sendErrorEmails($error, $data['transaction_details']);
+            return $error;
+        }
+
+        // if here, all went well
+        $error = 'processed';
+        return $error;
+    }
+
+    /**
+     * Processes the sale payment
+     *
+     * @param array $data IPN data
+     * @return boolean Did the IPN Validate?
+     * @access protected
+     */
+    function _processSale( $data, $ipnValidationFailed='' )
+    {
+        $send_email = false;
+        /*
+         * validate the payment data
+         */
+        $errors = array();
+
+        if (!empty($ipnValidationFailed))
+        {
+            $errors[] = $ipnValidationFailed;
+        }
+
+        // is the recipient correct?
+        if (empty($data['receiver_email']) || $data['receiver_email'] != $this->_getParam( 'merchant_email' )) {
+            $errors[] = JText::_('PAYPAL MESSAGE RECEIVER INVALID');
+        }
+
+        // load the orderpayment record and set some values
+        JTable::addIncludePath( JPATH_ADMINISTRATOR.DS.'components'.DS.'com_tienda'.DS.'tables' );
+        $orderpayment = JTable::getInstance('OrderPayments', 'TiendaTable');
+        $orderpayment->load( $data['custom'] );
+        $orderpayment->transaction_details  = $data['transaction_details'];
+        $orderpayment->transaction_id       = $data['txn_id'];
+        $orderpayment->transaction_status   = $data['payment_status'];
+
+        // check the stored amount against the payment amount
+        $stored_amount = number_format( $orderpayment->get('orderpayment_amount'), '2' );
+        if ((float) $stored_amount !== (float) $data['mc_gross']) {
+            $errors[] = JText::_('PAYPAL MESSAGE AMOUNT INVALID');
+        }
+
+        // check the payment status
+        if (empty($data['payment_status']) || ($data['payment_status'] != 'Completed' && $data['payment_status'] != 'Pending')) {
+            $errors[] = JText::sprintf('PAYPAL MESSAGE STATUS INVALID', @$data['payment_status']);
+        }
+
+        // set the order's new status and update quantities if necessary
+        JLoader::import( 'com_tienda.helpers.order', JPATH_ADMINISTRATOR.DS.'components' );
+        JLoader::import( 'com_tienda.helpers.carts', JPATH_ADMINISTRATOR.DS.'components' );
+        $order = JTable::getInstance('Orders', 'TiendaTable');
+        $order->load( $orderpayment->order_id );
+        if (count($errors))
+        {
+            // if an error occurred
+            $order->order_state_id = $this->params->get('failed_order_state', '10'); // FAILED
+        }
+            elseif (@$data['payment_status'] == 'Pending')
+        {
+            // if the transaction has the "pending" status,
+            $order->order_state_id = TiendaConfig::getInstance('pending_order_state', '1'); // PENDING
+            // Update quantities for echeck payments
+            TiendaHelperOrder::updateProductQuantities( $orderpayment->order_id, '-' );
+
+            // remove items from cart
+            TiendaHelperCarts::removeOrderItems( $orderpayment->order_id );
+        }
+            else
+        {
+            $order->order_state_id = $this->params->get('payment_received_order_state', '17');; // PAYMENT RECEIVED
+            
+            // do post payment actions
+            $setOrderPaymentReceived = true;
+            
+            // send email
+            $send_email = true;
+        }
+
+        // save the order
+        if (!$order->save())
+        {
+            $errors[] = $order->getError();
+        }
+
+        // save the orderpayment
+        if (!$orderpayment->save())
+        {
+            $errors[] = $orderpayment->getError();
+        }
+        
+        if (!empty($setOrderPaymentReceived))
+        {
+            $this->setOrderPaymentReceived( $orderpayment->order_id );
+        }
+        
+        if ($send_email)
+        {
+            // send notice of new order
+            Tienda::load( "TiendaHelperBase", 'helpers._base' );
+            $helper = TiendaHelperBase::getInstance('Email');
+            $model = Tienda::getClass("TiendaModelOrders", "models.orders");
+            $model->setId( $orderpayment->order_id );
+            $order = $model->getItem();
+            $helper->sendEmailNotices($order, 'new_order');
+        }
+
+        return count($errors) ? implode("\n", $errors) : '';
+    }
+
+    /**
+     * Formatts the payment data for storing
+     *
+     * @param array $data
+     * @return string
+     */
+    function _getFormattedTransactionDetails( $data )
+    {
+        $separator = "\n";
+        $formatted = array();
+
+        foreach ($data as $key => $value)
+        {
+            if ($key != 'view' && $key != 'layout')
+            {
+                $formatted[] = $key . ' = ' . $value;
+            }
+        }
+
+        return count($formatted) ? implode("\n", $formatted) : '';
+    }
+
+    /**
+     * Sends error messages to site administrators
+     *
+     * @param string $message
+     * @param string $paymentData
+     * @return boolean
+     * @access protected
+     */
+    function _sendErrorEmails($message, $paymentData)
+    {
+        $mainframe =& JFactory::getApplication();
+
+        // grab config settings for sender name and email
+        $config     = &TiendaConfig::getInstance();
+        $mailfrom   = $config->get( 'emails_defaultemail', $mainframe->getCfg('mailfrom') );
+        $fromname   = $config->get( 'emails_defaultname', $mainframe->getCfg('fromname') );
+        $sitename   = $config->get( 'sitename', $mainframe->getCfg('sitename') );
+        $siteurl    = $config->get( 'siteurl', JURI::root() );
+
+        $recipients = $this->_getAdmins();
+        $mailer =& JFactory::getMailer();
+
+        $subject = JText::sprintf('PAYPAL EMAIL PAYMENT NOT VALIDATED SUBJECT', $sitename);
+
+        foreach ($recipients as $recipient)
+        {
+            $mailer = JFactory::getMailer();
+            $mailer->addRecipient( $recipient->email );
+
+            $mailer->setSubject( $subject );
+            $mailer->setBody( JText::sprintf('PAYPAL EMAIL PAYMENT FAILED BODY', $recipient->name, $sitename, $siteurl, $message, $paymentData) );
+            $mailer->setSender(array( $mailfrom, $fromname ));
+            $sent = $mailer->send();
+        }
+
+        return true;
+    }
+
+    /**
+     * Gets admins data
+     *
+     * @return array|boolean
+     * @access protected
+     */
+    function _getAdmins()
+    {
+        $db =& JFactory::getDBO();
+        $q = "SELECT name, email FROM #__users "
+           . "WHERE LOWER(usertype) = \"super administrator\" "
+           . "AND sendEmail = 1 "
+           ;
+        $db->setQuery($q);
+        $admins = $db->loadObjectList();
+
+        if ($error = $db->getErrorMsg()) {
+            JError::raiseError(500, $error);
+            return false;
+        }
+
+        return $admins;
+    }
+
+}
+
---
+++ auxiliary_extensions/tienda_plugin_payment_virtualmerchant/payment_virtualmerchant/tmpl/prepayment.php
@@ -0,0 +1,26 @@
+<?php defined('_JEXEC') or die('Restricted access'); ?>
+
+<form action="<?php echo @$vars->payment_url; ?>" method="post" name="adminForm" enctype="multipart/form-data">
+
+    <input type='hidden' name='ssl_merchant_id' value='<?php echo @$vars->ssl_merchant_id; ?>' />
+    <input type='hidden' name='ssl_pin' value='<?php echo @$vars->ssl_pin; ?>' />
+    
+    <input type='hidden' name='ssl_amount' value='<?php echo @$vars->amount; ?>' />
+    
+    <?php if(@$vars->test_mode):?>
+    <input type='hidden' name='ssl_test_mode' value='true' />
+    <?php else: ?>
+    <input type='hidden' name='ssl_test_mode' value='false' />
+    <?php endif;?>
+
+    <input type="submit" class="button" value="<?php echo JText::_('Click Here to Complete Order'); ?>" />
+
+    <input type='hidden' name='order_id' value='<?php echo @$vars->order_id; ?>' />
+    <input type='hidden' name='orderpayment_id' value='<?php echo @$vars->orderpayment_id; ?>' />
+    <input type='hidden' name='orderpayment_type' value='<?php echo @$vars->orderpayment_type; ?>' />
+    
+    <input type='hidden' name='ssl_receipt_link_method' value='GET' />
+    <input type='hidden' name='ssl_receipt_link_url' value='<?php echo JRoute::_(@$vars->receipt_url); ?>' />
+    
+    <?php echo JHTML::_( 'form.token' ); ?>
+</form>
