151d0d8e2319832e7eeb57c856c2bc964f1a2ad6
geomaneo <geomaneo@dioscouri.com>
2011-01-27 21:35:08 +0000

    convert service list to array if only one service is selected to remove error notice. Multilist params returns a string if selected only one - refs #4427

--- tienda/tienda_plugin_shipping_ups/shipping_ups.php
+++ tienda/tienda_plugin_shipping_ups/shipping_ups.php
@@ -336,8 +336,16 @@ class plgTiendaShipping_ups extends TiendaShippingPlugin
     function getServices()
     {
         $upsServices = $this->getUpsServices();
-        $services = array(); 
+        $services = array();        
         $services_list = $this->params->get( 'services' );
+        //check if array
+        if(!is_array($services_list))
+        {
+        	//convert to array
+        	//it happens when only 1 service is selected
+        	$services_list = array($services_list);
+        }
+        
         foreach ($services_list as $service)
         {
             if (array_key_exists($service, $upsServices))
