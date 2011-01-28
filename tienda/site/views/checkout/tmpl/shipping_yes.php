fa9b75182191a6ce8e4321a7cbaa14f80959d54c
geomaneo <geomaneo@dioscouri.com>
2011-01-28 21:17:06 +0000

    set values to the shipping hidden fields if default rate is available - closes #4429

--- tienda/site/views/checkout/tmpl/shipping_yes.php
+++ tienda/site/views/checkout/tmpl/shipping_yes.php
@@ -9,16 +9,13 @@
 
 <?php
     if (!empty($this->rates)) 
-    {                 
-        //$dfltShippingMethod = TiendaConfig::getInstance()->get('defaultShippingMethod', '2');
-        //$checkAlready = false;
-		
+    {      
         foreach ($this->rates as $rate) 
         {
             $checked = "";
             if (!empty($this->default_rate) && $this->default_rate['name'] == $rate['name'] )
             {
-                $checked = "checked";
+            	$checked = "checked";                        
             }        	        		
             ?>
             <input name="shipping_plugin" type="radio" value="<?php echo $rate['element'] ?>" onClick="tiendaSetShippingRate('<?php echo $rate['name']; ?>','<?php echo $rate['price']; ?>',<?php echo $rate['tax']; ?>,<?php echo $rate['extra']; ?>, '<?php echo $rate['code']; ?>');" <?php echo $checked; ?> /> <?php echo $rate['name']; ?> ( <?php echo TiendaHelperBase::currency( $rate['total'] ); ?> )<br />
@@ -35,12 +32,13 @@
         <?php
     }
 ?>
-<input type="hidden" name="shipping_price" id="shipping_price" value="" />
-<input type="hidden" name="shipping_tax" id="shipping_tax" value="" />
-<input type="hidden" name="shipping_name" id="shipping_name" value="" />
-<input type="hidden" name="shipping_code" id="shipping_code" value="" />
-<input type="hidden" name="shipping_extra" id="shipping_extra" value="" />
-
+<?php $setval = false;?>
+<?php if(count($this->rates)==1 && ($this->rates['0']['name'] == $this->default_rate['name'])) $setval= true;?>
+<input type="hidden" name="shipping_price" id="shipping_price" value="<?php echo $setval ? $this->rates['0']['price'] : "";?>" />
+<input type="hidden" name="shipping_tax" id="shipping_tax" value="<?php echo $setval ? $this->rates['0']['tax'] : "";?>" />
+<input type="hidden" name="shipping_name" id="shipping_name" value="<?php echo $setval ? $this->rates['0']['name'] : "";?>" />
+<input type="hidden" name="shipping_code" id="shipping_code" value="<?php echo $setval ? $this->rates['0']['code'] : "";?>" />
+<input type="hidden" name="shipping_extra" id="shipping_extra" value="<?php echo $setval ? $this->rates['0']['extra'] : "";?>" />
 <div id='shipping_form_div' style="padding-top: 10px;"></div>
     
 <!--    COMMENTS   -->     
