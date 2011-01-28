088fdb853f1add7b40bf4b05d8979a14fd173ebc
geomaneo <geomaneo@dioscouri.com>
2011-01-28 14:32:18 +0000

    get the shipper number from plugin params - refs #4427

--- tienda/tienda_plugin_shipping_ups/shipping_ups/ups.php
+++ tienda/tienda_plugin_shipping_ups/shipping_ups/ups.php
@@ -283,7 +283,8 @@ class TiendaUpsRate extends TiendaUps
                     'PostalCode' => $this->originPostalCode,
                     'CountryCode' => $this->originCountryCode,
 			),
-			'ShipperNumber' => 'V3443E',
+			//'ShipperNumber' => 'V3443E',
+			'ShipperNumber' => $this->shipperNumber,
 		);        
         $request['Shipment']['ShipTo'] = array(
             'Address' => 
