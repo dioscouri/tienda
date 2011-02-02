46dffff7f26a1203e161df40b8d7cdd0abf81c77
rowelld <rowelld@dioscouri.com>
2011-02-02 21:15:26 +0000

    First Draft Admin-side User Dashboard View refs #4450

--- tienda/admin/controllers/users.php
+++ tienda/admin/controllers/users.php
@@ -48,6 +48,54 @@ class TiendaControllerUsers extends TiendaController
         }
         return $state;
     }
+    
+	function view()
+	{
+		$model = $this->getModel( $this->get('suffix') );
+		$model->getId();
+		$row = $model->getItem();
+		//debug(221122,$row);
+		$view   = $this->getView( $this->get('suffix'), 'html' );
+		$view->setModel( $model, true );
+		$view->assign( 'row', $row );
+		$view->setLayout( 'view' );
+		
+		//Get Data From Carts Model
+		JTable::addIncludePath( JPATH_ADMINISTRATOR.DS.'components'.DS.'com_tienda'.DS.'tables' );
+		$modelCarts = JModel::getInstance( 'Carts', 'TiendaModel');			
+		$modelCarts->setState('filter_user', $row->id );	
+		$carts = $modelCarts->getList();
+		$view->assign( 'carts', $carts );						
+		 foreach (@$carts as $cart)
+		 {
+				$cart->total_price=$cart->product_price *$cart->product_qty;
+		 }
+		 
+		 //Get Data from Productcomments Model and left join to products
+		$database = $model->getDbo();
+		Tienda::load( 'TiendaQuery', 'library.query' );
+        $query = new TiendaQuery();
+        $query->select( 'tbl.*');
+        $query->select('p.product_name AS p_name');
+        $query->join('LEFT', '#__tienda_products AS p ON p.product_id = tbl.product_id');   
+        $query->select( 'substring(tbl.productcomment_text, 1, 250) AS trimcom' );
+        $query->from( '#__tienda_productcomments AS tbl' );
+        $query->where("tbl.user_id='$row->id' LIMIT 5");        
+        $database->setQuery( (string) $query );
+        $procoms = $database->loadObjectList();
+		$view->assign( 'procoms', $procoms);
+		//debug(2222,$procoms);
+		
+		
+		$model->emptyState();
+		$this->_setModelState();
+		$surrounding = $model->getSurrounding( $model->getId() );
+		$view->assign( 'surrounding', $surrounding );
+
+		$view->display();
+		$this->footer();
+		return;
+	}
 }
 
 ?>
--- tienda/admin/views/users/tmpl/view.php
+++ tienda/admin/views/users/tmpl/view.php
@@ -1,128 +1,209 @@
 <?php defined('_JEXEC') or die('Restricted access'); ?>
 <?php $form = @$this->form; ?>
 <?php $row = @$this->row; ?>
+<?php $carts = @$this->carts; ?>
+<?php $procoms=@$this->procoms; ?>
 
 <form action="<?php echo JRoute::_( @$form['action'] )?>" method="post" name="adminForm" enctype="multipart/form-data">
-
-    <?php echo TiendaGrid::pagetooltip( 'users_view' ); ?>
-
-    <fieldset>
-        <legend><?php echo JText::_('User'); ?></legend>
-        <div id="tienda_header">
-            <table class="admintable" style="width: 99%;">
-                <tr>
-                    <td width="100" align="right" class="key">
-                        <label for="name">
-                        <?php echo JText::_( 'Name' ); ?>:
-                        </label>
-                    </td>
-                    <td>
-                        <div class="name"><?php echo @$row->name; ?></div>          
-                    </td>
-                    <td width="100" align="right" class="key">
-                        <label for="registerDate">
-                        <?php echo JText::_( 'Registered' ); ?>:
-                        </label>
-                    </td>
-                    <td>
-                        <div class="registerDate"><?php echo JHTML::_('date', @$row->registerDate, "%a, %d %b %Y, %H:%M"); ?></div>         
-                    </td>
-                    <td>
-                        <?php
-                        $config = TiendaConfig::getInstance();
-                        $url = $config->get( "user_edit_url", "index.php?option=com_users&view=user&task=edit&cid[]=");
-                        $url .= @$row->id; 
-                        $text = "<button>".JText::_('Click Here to Edit User')."</button>"; 
-                        ?>
-                        <div style="float: right;"><?php echo TiendaUrl::popup( $url, $text, array('update' => true) ); ?></div>
-                    </td>
-                </tr>
-                <tr>
-                    <td width="100" align="right" class="key">
-                        <label for="username">
-                        <?php echo JText::_( 'Username' ); ?>:
-                        </label>
-                    </td>
-                    <td>
-                        <div class="username"><?php echo @$row->username; ?></div>          
-                    </td>
-                    <td width="100" align="right" class="key">
-                        <label for="lastvisitDate">
-                        <?php echo JText::_( 'Last Visited' ); ?>:
-                        </label>
-                    </td>
-                    <td>
-                        <div class="lastvisitDate"><?php echo JHTML::_('date', @$row->lastvisitDate, "%a, %d %b %Y, %H:%M"); ?></div>           
-                    </td>
-                    <td>
-                    </td>
-                </tr>
-                <tr>
-                    <td width="100" align="right" class="key">
-                        <label for="email">
-                        <?php echo JText::_( 'Email' ); ?>:
-                        </label>
-                    </td>
-                    <td>
-                        <div class="email"><?php echo @$row->email; ?></div>            
-                    </td>
-                    <td>
-                    </td>
-                    <td>
-                    </td>
-                    <td>
-                    </td>
-                </tr>
-                <tr>
-                    <td width="100" align="right" class="key">
-                        <label for="id">
-                        <?php echo JText::_( 'ID' ); ?>:
-                        </label>
-                    </td>
-                    <td>
-                        <div class="id"><?php echo @$row->id; ?></div>          
-                    </td>
-                    <td>
-                    </td>
-                    <td>
-                    </td>
-                    <td>
-                    </td>
-                </tr>
-            </table>
-        </div>
-    </fieldset>
-    
-    <table style="width: 100%;">
-    <tr>
-        <td style="width: 70%; max-width: 70%; vertical-align: top; padding: 0px 5px 0px 5px;">
-            <?php
-            $modules = JModuleHelper::getModules("tienda_user_main");
-            $document   = &JFactory::getDocument();
-            $renderer   = $document->loadRenderer('module');
-            $attribs    = array();
-            $attribs['style'] = 'xhtml';
-            foreach ( @$modules as $mod ) 
-            {
-                echo $renderer->render($mod, $attribs);
-            }
-            ?>
-        </td>
-        <td style="vertical-align: top; width: 30%; min-width: 30%; padding: 0px 5px 0px 5px;">     
-            <?php
-            $modules = JModuleHelper::getModules("tienda_user_right");
-            $document   = &JFactory::getDocument();
-            $renderer   = $document->loadRenderer('module');
-            $attribs    = array();
-            $attribs['style'] = 'xhtml';
-            foreach ( @$modules as $mod ) 
-            {
-                echo $renderer->render($mod, $attribs);
-            }
-            ?>
-        </td>
-    </tr>
-    </table>
+<h2 style="padding:0px; margin:0px;"><?php echo @$row->first_name; ?> <?php echo @$row->middle_name; ?> <?php echo @$row->last_name; ?></h2>
+<?php echo TiendaGrid::pagetooltip( 'users_view' ); ?>
+<table width="100%" border="0">
+	<tr>
+		<td width="50%">
+			<fieldset>
+				<legend><?php echo JText::_('Basic User Info'); ?></legend>
+				<div id="tienda_header">
+					<table class="admintable" style="width: 100%;">					
+						<tr>
+							<td  align="right" class="key" style="width:65px;">
+		                        <label for="name">
+		                        	<?php echo JText::_( 'Username' ); ?>:
+		                        </label>
+	                    	</td>
+	                    	<td>
+	                        	<div class="name"><?php echo @$row->username; ?></div>          
+	                    	</td>
+	                    	<td align="right" class="key" key" style="width:65px;">
+		                        <label for="email">
+		                        	<?php echo JText::_( 'Email' ); ?>:
+		                        </label>
+	                    	</td>
+	                    	<td>
+	                        	<div class="name"><?php echo @$row->email; ?></div>          
+	                    	</td>   
+	                    	<td  align="right" class="key" key" style="width:65px;">
+		                        <label for="id">
+		                        	<?php echo JText::_( 'ID' ); ?>:
+		                        </label>
+		                    </td>
+		                    <td>
+		                        <div class="id"><?php echo @$row->id; ?></div>          
+		                    </td>
+	                    	<td>
+		                        <?php
+		                        $config = TiendaConfig::getInstance();
+		                        $url = $config->get( "user_edit_url", "index.php?option=com_users&view=user&task=edit&cid[]=");
+		                        $url .= @$row->id; 
+		                        $text = "<button>".JText::_('Edit User')."</button>"; 
+		                        ?>		                        
+		                        <div style="float: right;"><?php echo TiendaUrl::popup( $url, $text, array('update' => true) ); ?></div>
+		                    </td>                 	
+						</tr>
+						<tr>
+							<td  align="right" class="key" style="width:65px;">
+		                        <label for="registerDate">
+		                        	<?php echo JText::_( 'Registered' ); ?>:
+		                        </label>
+		                    </td>
+		                    <td colspan="2">
+		                        <div class="registerDate"><?php echo JHTML::_('date', @$row->registerDate, "%a, %d %b %Y, %H:%M"); ?></div>         
+		                    </td>
+		                    <td align="right" class="key">
+		                        <label for="lastvisitDate">
+		                        	<?php echo JText::_( 'Last Visited' ); ?>:
+		                        </label>
+		                    </td>
+		                    <td colspan="3">
+		                        <div class="lastvisitDate"><?php echo JHTML::_('date', @$row->lastvisitDate, "%a, %d %b %Y, %H:%M"); ?></div>           
+		                    </td>
+						</tr>
+						<tr>
+							<td align="right" class="key" style="width:65px;">
+		                        <label for="group_name">
+		                        	<?php echo JText::_( 'User Group' ); ?>:
+		                        </label>
+		                    </td>
+		                    <td colspan="3">
+		                      	<div class="id"><?php echo @$row->group_name; ?></div>     
+		                      	<table class="admintable" style="width: 100%;">
+		                      		<tr>
+		                      			<td>
+		                      			</td>
+		                      		</tr>		
+		                      	</table>     
+		                    </td>
+						</tr>
+					</table>
+				</div>
+			</fieldset>
+			
+				<fieldset>
+					<legend><?php echo JText::_('Summary Data'); ?></legend>
+					
+				</fieldset>
+			
+			<fieldset>
+					<legend><?php echo JText::_('Last 5 Completed Orders'); ?></legend>
+					
+				</fieldset>
+		</td>
+		<td width="50%">
+			<fieldset>
+					<legend><?php echo JText::_('Integration'); ?></legend>
+					
+				</fieldset>
+			<fieldset>
+					<legend><?php echo JText::_('List of Active Subscriptions'); ?></legend>
+					
+				</fieldset>
+			<fieldset>
+					<legend><?php echo JText::_('Cart'); ?></legend>
+					<table class="adminlist" style="width: 100%;">
+						<thead>
+							<tr>
+								<th style="width: 5px;">
+									<?php echo JText::_("Num"); ?>
+								</th>
+								<th style="width: 200px;">
+									<?php echo JText::_("Products"); ?>
+								</th>
+								<th style="width: 200px;">
+									<?php echo JText::_("Price"); ?>
+								</th>
+								<th style="text-align: center;  width: 200px;">
+									<?php echo JText::_("Quantity"); ?>
+								</th>
+								<th style="width: 150px; text-align: right;">
+									<?php echo JText::_("Total"); ?>
+								</th>
+							</tr>
+						</thead>
+						<tfoot>
+							<tr>
+								<td colspan="20"></td>
+							</tr>
+						</tfoot>
+						<tbody>
+							<?php $i=0; $k=0; ?>
+							<?php foreach (@$carts as $cart) : ?>
+								<tr class='row <?php echo $k; ?>'>
+									<td align="center">
+										<?php echo $i + 1; ?>
+									</td>
+									<td style="text-align:center;">
+										<a href="index.php?option=com_tienda&view=products&task=edit&id=<?php echo $cart->product_id; ?>" target="_blank">
+											<?php echo $cart->product_name; ?>
+										</a>
+									</td>
+									<td style="text-align:right;">
+										<?php echo TiendaHelperBase::currency($cart->product_price); ?>										
+									</td>
+									<td style="text-align:center;">
+										<?php echo $cart->product_qty;?>
+									</td>
+									<td style="text-align:right;">
+										<?php echo TiendaHelperBase::currency($cart->total_price); ?>										
+									</td>
+								</tr>
+							<?php ++$i; $k = (1 - $k); ?>
+							<?php endforeach; ?>
+						</tbody>
+					</table>
+				</fieldset>
+			<fieldset>
+					<legend><?php echo JText::_('Last 5 Reviews Posted'); ?></legend>
+					<table class="adminlist" style="width: 100%;">
+						<thead>
+							<tr>
+								<th style="width: 5px;">
+									<?php echo JText::_("Num"); ?>
+								</th>
+								<th>
+									<?php echo JText::_("Products + Comments"); ?>
+								</th>
+								<th style="width: 200px;">
+									<?php echo JText::_("User Rating"); ?>
+								</th>													
+							</tr>
+						</thead>		
+						<tfoot>
+							<tr>
+								<td colspan="20"></td>
+							</tr>
+						</tfoot>
+						<tbody>
+							<?php $i=0; $k=0; ?>
+							<?php foreach (@$procoms as $procom) : ?>
+								<tr class='row <?php echo $k; ?>'>
+									<td align="center">
+										<?php echo $i + 1; ?>
+									</td>
+									<td style="text-align:left;">
+										<a href="index.php?option=com_tienda&view=products&task=edit&id=<?php echo $procom->product_id; ?>" target="_blank">
+											<?php echo $procom->p_name; ?></a><br/><?php echo $procom->trimcom; ?>							
+									</td>
+									<td style="text-align:center;">
+										<?php echo TiendaHelperProduct::getRatingImage( @$procom->productcomment_rating ); ?>										
+									</td>
+								</tr>
+							<?php ++$i; $k = (1 - $k); ?>
+							<?php endforeach; ?>
+						</tbody>	
+					</table>
+				</fieldset>
+		</td>
+	</tr>
+</table>
     
     <input type="hidden" name="id" value="<?php echo @$row->id; ?>" />
     <input type="hidden" name="task" value="" />
