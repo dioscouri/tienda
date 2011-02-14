b89586c64ddd2ab123e41950bb9ea96ad331dc03
geomaneo <geomaneo@dioscouri.com>
2011-02-14 12:04:29 +0000

    closes #4372 - Google Analytics eCommerce Tracking

--- tienda/tienda_plugin_googleanalytics/googleanalytics.php
+++ tienda/tienda_plugin_googleanalytics/googleanalytics.php
@@ -20,9 +20,7 @@ class plgTiendaGoogleAnalytics extends TiendaPluginBase
 	 */
     public $_element    	= 'googleanalytics';    
     private $webid 			= '';        
-    private $inccategory 	= '0';    
-    private $embed 			= false;    
-    private $order 			= null;
+    private $inccategory 	= '0';  
     
 	function plgTiendaGoogleAnalytics(& $subject, $config) 
 	{
@@ -37,51 +35,35 @@ class plgTiendaGoogleAnalytics extends TiendaPluginBase
 	 * @param $order
 	 */
 	function onAfterDisplayPrePayment($order)
-	{	
-		$this->embed = true;
-		$this->order = $order;		
+	{			
+			$doc = JFactory::getDocument();		
+			$script = $this->_buildScript($order);	
+			$doc->addScriptDeclaration($script);
 	}
-	
-	/**
-	 * This event is triggered after the framework has rendered the application. 
-	 */
- 	function onAfterRender ()
-    {   
-    	if($this->embed)
-    	{        
-        	JResponse::setBody(str_replace("</body>", $this->_buildScript() . "\r\n</body>", JResponse::getBody()));
-    	}
-    }
-	
-	
+		
 	/**
 	 * Method to build the script to be appended to the body	
+	 * @param object $order - order object 
 	 * @return string
 	 */
-	function _buildScript()
-	{
-		$order = $this->order;
+	function _buildScript($order)
+	{		
 		$storeName = TiendaConfig::getInstance()->get('shop_name');
 		$tax = $order->order_tax + $order->order_shipping_tax;
 		
-		$script = "";
-		$script .= "<script type=\"text/javascript\">\r\n";
-		$script .= "var gaJsHost = ((\"https:\" == document.location.protocol ) ? \"https://ssl.\" : \"http://www.\");\r\n";
-		$script .= "document.write(unescape(\"%3Cscript src='\" + gaJsHost + \"google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E\"));\r\n";
-		$script .= "</script>\r\n";  
-		$script .= "<script type=\"text/javascript\">\r\n";
-		$script .= "try{\r\n";
-		$script .= "var pageTracker = _gat._getTracker(\"{$this->webid}\");\r\n";
-		$script .= "pageTracker._trackPageview();\r\n";
-		$script .= "pageTracker._addTrans(\r\n";
-		$script .= "		{$order->order_id}\r\n";
-		$script .= "		{$storeName}\r\n";
-		$script .= "		{$order->order_total}\r\n";
-		$script .= "		{$tax}\r\n";
-		$script .= "		{$order->orderinfo->billing_city}\r\n";
-		$script .= "		{$order->orderinfo->billing_zone_name}\r\n";
-		$script .= "		{$order->orderinfo->billing_country_name}\r\n";
-		$script .= "		);\r\n\r\n\r\n";
+		$script = "";		
+		$script .= "var _gaq = _gaq || [];\r\n";
+		$script .= "_gaq.push(['_setAccount', '{$this->webid}']);\r\n";
+		$script .= "_gaq.push(['_trackPageview']);\r\n";
+		$script .= "_gaq.push(['_addTrans',\r\n";
+		$script .= "	'{$order->order_id}',\r\n";
+		$script .= "	'{$storeName}',\r\n";
+		$script .= "	'{$order->order_total}',\r\n";
+		$script .= "	'{$tax}',\r\n";
+		$script .= "	'{$order->orderinfo->billing_city}',\r\n";
+		$script .= "	'{$order->orderinfo->billing_zone_name}',\r\n";
+		$script .= "	'{$order->orderinfo->billing_country_name}'\r\n";
+		$script .= "]);\r\n\r\n\r\n";	
 				
 		Tienda::load( "TiendaHelperProduct", 'helpers.product' );
 		$items = $order->getItems();		 
@@ -94,20 +76,23 @@ class plgTiendaGoogleAnalytics extends TiendaPluginBase
 				$catName = $this->_getCategoryName($item->product_id);
 		 	}
 		 	
-		 	$script .= "pageTracker._addItem(\r\n";
-		 	$script .= "		{$order->order_id}\r\n";
-		 	$script .= "		{$item->product_id}\r\n";
-		 	$script .= "		{$item->orderitem_name}\r\n";
-		 	$script .= "		{$catName}\r\n";
-		 	$script .= "		{$item->orderitem_final_price}\r\n";
-		 	$script .= "		{$item->orderitem_quantity}\r\n";
-		 	$script .= ");\r\n";
+		 	$script .= "_gaq.push(['_addItem',\r\n";
+		 	$script .= "	'{$order->order_id}',\r\n";
+		 	$script .= "	'{$item->product_id}',\r\n";
+		 	$script .= "	'{$item->orderitem_name}',\r\n";
+		 	$script .= "	'{$catName}',\r\n";
+		 	$script .= "	'{$item->orderitem_final_price}',\r\n";
+		 	$script .= "	'{$item->orderitem_quantity}',\r\n";
+		 	$script .= "]);\r\n";
 		 }
 		 		 
-		$script .= "pageTracker._trackTrans();\r\n";
-		$script .= "} catch(err) {}\r\n";
-		$script .= "</script>\r\n"; 
-			 		 		
+		$script .= "_gaq.push(['_trackTrans']);\r\n";
+		$script .= "(function() {\r\n";
+		$script .= "var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;\r\n";
+		$script .= "ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';\r\n";
+		$script .= "var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);\r\n";
+		$script .= "})();\r\n";
+					 		 		
 		return $script;
 	}
 	
