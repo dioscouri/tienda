e604a75237ffc16d780d49fb4a051138ef7b3541
geomaneo <geomaneo@dioscouri.com>
2011-03-31 18:09:48 +0000

    dont show headings if onepage checkout - refs #785

--- tienda/site/views/checkout/tmpl/shipping_calculate.php
+++ tienda/site/views/checkout/tmpl/shipping_calculate.php
@@ -1,6 +1,7 @@
 <?php defined('_JEXEC') or die('Restricted access'); ?>
-
+<?php  if (!TiendaConfig::getInstance()->get('one_page_checkout')) :?>
 <h3><?php echo JText::_("Select a Shipping Method") ?></h3>
+<?php endif;?>
 <input type="button" onclick="tiendaGetShippingRates( 'onCheckoutShipping_wrapper', this.form )" value="<?php echo JText::_("Click here to determine your shipping rates"); ?>" />
 
 <input type="hidden" id="shippingrequired" name="shippingrequired" value="1"  />
