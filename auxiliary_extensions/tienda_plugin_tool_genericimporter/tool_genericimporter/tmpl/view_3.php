68b89cdc0f8d8789f7c190c2bc5114c069237444
rowelld <rowelld@dioscouri.com>
2011-05-16 22:35:42 +0000

    fixes #5437 Import Coupons from CSV... Migration Result

--- auxiliary_extensions/tienda_plugin_tool_genericimporter/tool_genericimporter/tmpl/view_3.php
+++ auxiliary_extensions/tienda_plugin_tool_genericimporter/tool_genericimporter/tmpl/view_3.php
@@ -1,8 +1,14 @@
-<?php defined('_JEXEC') or die('Restricted access'); ?>
-<p><?php echo JText::_( $this->_importer->get( 'tool_description' ) ); ?></p>
+<?php	defined('_JEXEC') or die('Restricted access');?>
+<p>
+	<?php	echo JText::_($this->_importer->get('tool_description'));?>
+</p>
 <div class="note">
-	<span style="float: right; font-size: large; font-weight: bold;"><?php echo JText::_( "FINAL" ); ?></span>
-	<p><?php echo JText::_( "MIGRATION RESULTS"); ?></p>
+	<span style="float: right; font-size: large; font-weight: bold;">
+		<?php	echo JText::_("FINAL");?>
+	</span>
+	<p>
+		<?php	echo JText::_("MIGRATION RESULTS");?>
+	</p>
 </div>
-<?php  echo $this->getHtmlStep( 3, 2); ?>
-<?php echo $this->vars->additional_html; ?>
+<?phpecho $this->getHtmlStep(3, 2);?>
+<?phpecho $this->vars->additional_html;?>
--- auxiliary_extensions/tienda_plugin_tool_genericimporter/tool_genericimporter/types/import_csvcoupons.php
+++ auxiliary_extensions/tienda_plugin_tool_genericimporter/tool_genericimporter/types/import_csvcoupons.php
@@ -11,10 +11,9 @@
 /** ensure this file is being included by a parent file */
 defined('_JEXEC') or die('Restricted access');
 
-require_once JPATH_SITE.'/plugins/tienda/tool_genericimporter/genericimport_csv.php';
+require_once JPATH_SITE . '/plugins/tienda/tool_genericimporter/genericimport_csv.php';
 
-class plgTiendaTool_CsvCoupons extends TiendaToolPluginImportCsv
-{
+class plgTiendaTool_CsvCoupons extends TiendaToolPluginImportCsv {
 	/*
 	 * Name of the importer displayed in select box
 	 */
@@ -22,19 +21,18 @@ class plgTiendaTool_CsvCoupons extends TiendaToolPluginImportCsv
 
 	var $keys = array();
 
+	var $results = array();
+
+	var $_element = 'tool_genericimporter';
+
 	/*
 	 * Sets default values for variables from request
 	 */
-	function getDefaultState()
-	{
+	function getDefaultState() {
 		parent::getDefaultState();
 		$this->import_throttled_import = true;
 		$this->import_num_records = 1;
-		$this->keys = array(
-		'coupon_id', 'coupon_name', 'coupon_code', 'coupon_type', 'coupon_group', 'coupon_automatic', 'coupon_value', 'coupon_value_type', 'currency_id',
-		'coupon_description', 'coupon_params', 'created_date', 'modified_date', 'start_date', 'expiration_date', 'coupon_enabled', 'coupon_uses', 'coupon_max_uses', 'coupon_max_uses_per_user'
-		);
-		$this->state->lol = '(party)';
+		$this->keys = array('coupon_id', 'coupon_name', 'coupon_code', 'coupon_type', 'coupon_group', 'coupon_automatic', 'coupon_value', 'coupon_value_type', 'currency_id', 'coupon_description', 'coupon_params', 'created_date', 'modified_date', 'start_date', 'expiration_date', 'coupon_enabled', 'coupon_uses', 'coupon_max_uses', 'coupon_max_uses_per_user');
 	}
 
 	/*
@@ -44,69 +42,124 @@ class plgTiendaTool_CsvCoupons extends TiendaToolPluginImportCsv
 	 *
 	 * @return String HTML code of the table
 	 */
-	function getAdminTableHtml( $step )
-	{
-		$state = $this->get( 'state' );
-		$checked = array( '', '\'checked\'' );
-		$answer = array( JText::_( 'No' ), JText::_( 'Yes' ) );
+	function getAdminTableHtml($step) {
+		$state = $this->get('state');
+		$checked = array('', '\'checked\'');
+		$answer = array(JText::_('No'), JText::_('Yes'));
 		$rows = array();
 		$only_value = $step == 2;
-		$skip_first_value = strcmp( $state->skip_first, 'on') == 0;
+		$skip_first_value = strcmp($state->skip_first, 'on') == 0;
 
-		$rows[] = $this->generateRowInput( $only_value, JText::_( 'Source Import' ).': *' ,'file', 'source_import', $this->source_import );
-		$rows[] = $this->generateRowInput( $only_value, JText::_( 'Separator' ) ,'text', 'field_separator', $state->field_separator, 10 );
-		$rows[] = $this->generateRowInput( $only_value, JText::_( 'Skip First Row' ).'?' ,'checkbox', 'skip_first', $skip_first_value, null, $answer[$skip_first_value], null, $checked[$skip_first_value] );
+		$rows[] = $this->generateRowInput($only_value, JText::_('Source Import') . ': *', 'file', 'source_import', $this->source_import);
+		$rows[] = $this->generateRowInput($only_value, JText::_('Separator'), 'text', 'field_separator', $state->field_separator, 10);
+		$rows[] = $this->generateRowInput($only_value, JText::_('Skip First Row') . '?', 'checkbox', 'skip_first', $skip_first_value, null, $answer[$skip_first_value], null, $checked[$skip_first_value]);
 
-		$this->set( 'table_rows', $rows );
-		return parent::getAdminTableHtml( $step );
+		$this->set('table_rows', $rows);
+		return parent::getAdminTableHtml($step);
 	}
-
-
 
 	/*
 	 * Performs the actual migration of data
 	 *
 	 * @return Additional HTML code you would like to display on the final step
 	 */
-	function migrate_data()
-	{
+	function migrate_data() {
 		$datas = $this->data;
 
-		JTable::addIncludePath( JPATH_ADMINISTRATOR . DS . 'components' . DS . 'com_tienda' . DS . 'tables' );
-		$coupon = JTable::getInstance( 'Coupons', 'TiendaTable' );
-		foreach($datas as $data)
-		{
+		//debug(23444,$this->data);
+		$result = '';
+
+		JTable::addIncludePath(JPATH_ADMINISTRATOR . DS . 'components' . DS . 'com_tienda' . DS . 'tables');
+		$coupon = JTable::getInstance('Coupons', 'TiendaTable');
+
+		$i = 0;
+		foreach($datas as $data) {
+
 			$fields = $this->mapFields($data);
 
 			$coupon->coupon_id = '';
 			$coupon->coupon_name = $fields['coupon_name'];
-			$coupon->coupon_code  = $fields['coupon_code'];
+			$coupon->coupon_code = $fields['coupon_code'];
 			$coupon->coupon_value = $fields['coupon_value'];
 			$coupon->coupon_value_type = $fields['coupon_value_type'];
 			$coupon->start_date = $fields['start_date'];
 			$coupon->coupon_max_uses = $fields['coupon_max_uses'];
 			$coupon->coupon_max_uses_per_user = $fields['coupon_max_uses_per_user'];
-			
-			if( !$coupon->save() )
-			{
-				JFactory::getApplication( )->enqueueMessage( $coupon->getError(), 'notice' );
-			} else {
-				JFactory::getApplication( )->enqueueMessage( JText::_('IMPORT SUCCESS'), '' );
-			}
+			$coupon->save();
+
+			$result = new stdClass();
+			$result->title = $coupon->coupon_name;
+			$result->errors = $coupon->getErrors();
+			$result->affectedRows = 1;
+			$i++;
+
+			$this->results[] = $result;
 		}
-		return Tienda::dump( $this->data );
+
 	}
 
-	function mapFields($datas)
-	{
-		$mapped = array( );
+	function mapFields($datas) {
+		$mapped = array();
 		$i = 0;
-		foreach ( $this->keys as $key )
-		{
+		foreach($this->keys as $key) {
 			$mapped[$key] = @$datas[$i];
 			$i++;
 		}
 
 		return $mapped;
 	}
+
+	/**
+	 *Show the Migration Results
+	 * return html code
+	 */
+	function getHtmlStep3View() {
+		$html = "";
+		$vars = new JObject();
+		$vars->results = $this->results;
+
+		$html = $this->_getLayout('view_coupon', $vars);
+
+		return $html;
+	}
+
+	function _getLayout($layout, $vars =false, $plugin ='', $group ='tienda') {
+		if(empty($plugin)) {
+			$plugin = $this->_element;
+		}
+
+		ob_start();
+		$layout = $this->_getLayoutPath($plugin, $group, $layout);
+		include ($layout);
+		$html = ob_get_contents();
+		ob_end_clean();
+
+		return $html;
+	}
+
+	/**
+	 * Get the path to a layout file
+	 *
+	 * @param   string  $plugin The name of the plugin file
+	 * @param   string  $group The plugin's group
+	 * @param   string  $layout The name of the plugin layout file
+	 * @return  string  The path to the plugin layout file
+	 * @access protected
+	 */
+	function _getLayoutPath($plugin, $group, $layout ='default') {
+		$app = JFactory::getApplication();
+
+		// get the template and default paths for the layout
+		$templatePath = JPATH_SITE . DS . 'templates' . DS . $app->getTemplate() . DS . 'html' . DS . 'plugins' . DS . $group . DS . $plugin . DS . $layout . '.php';
+		$defaultPath = JPATH_SITE . DS . 'plugins' . DS . $group . DS . $plugin . DS . 'tmpl' . DS . $layout . '.php';
+
+		// if the site template has a layout override, use it
+		jimport('joomla.filesystem.file');
+		if(JFile::exists($templatePath)) {
+			return $templatePath;
+		} else {
+			return $defaultPath;
+		}
+	}
+
 }
