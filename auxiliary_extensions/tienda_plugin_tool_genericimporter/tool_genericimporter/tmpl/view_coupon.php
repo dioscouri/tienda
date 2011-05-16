b9cd4b5dde8ba4ee2792f466c5a6fce5c5750c04
rowelld <rowelld@dioscouri.com>
2011-05-16 22:37:39 +0000

    refs #5437 forget to include the view_coupon.php from the last commit.

---
+++ auxiliary_extensions/tienda_plugin_tool_genericimporter/tool_genericimporter/tmpl/view_coupon.php
@@ -0,0 +1,65 @@
+<?php	defined('_JEXEC') or die('Restricted access');?>
+<?php	$items = @$vars->results;?>
+<table class="adminlist" style="clear: both;">
+	<thead>
+		<tr>
+			<th style="width: 5px;">
+			<?php	echo JText::_("NUM");?>
+			</th>
+			<th style="text-align: left;">
+			<?php	echo JText::_("TITLE");?>
+			</th>
+			<th style="width: 50px;">
+			<?php	echo JText::_("AFFECTED ROWS");?>
+			</th>
+			<th>
+			<?php	echo JText::_("ERRORS");?>
+			</th>
+		</tr>
+	</thead>
+	<tfoot>
+		<tr>
+			<td colspan="20">
+
+			</td>
+		</tr>
+	</tfoot>
+	<tbody>
+		<?php	$i = 0;
+			$k = 0;?>
+		<?php foreach (@$items as $item) : ?>
+		<tr class='row<?php	echo $k;?>'>
+			<td align="center">
+			<?php	echo $i + 1;?>
+			</td>
+			<td style="text-align: left;">
+			<?php	echo JText::_($item->title);?>
+			</td>
+			<td style="text-align: center;">
+			<?php	echo $item->affectedRows;?>
+			</td>
+			<td style="text-align: center;">
+			<ul>
+				<?php  foreach($item->errors as $error ) : ?>
+				<?php if(!empty($error)) : ?>
+				<li>
+					<?php	echo $error;?>
+				</li>
+				<?php	endif;?>
+				<?php	endforeach;?>
+			</ul>
+			</td>
+		</tr>
+		<?php	++$i;
+			$k = (1 - $k);?>
+		<?php	endforeach;?>
+
+		<?php if (!count(@$items)) : ?>
+		<tr>
+			<td colspan="10" align="center">
+			<?php	echo JText::_('NO ITEMS FOUND');?>
+			</td>
+		</tr>
+		<?phpendif;?>
+	</tbody>
+</table>
