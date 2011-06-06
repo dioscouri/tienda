2da71ebb96b203c4c222230ee0efac96b30722f8
bduckett <bduckett@dioscouri.com>
2011-06-06 20:18:43 +0000

    closes #5569

--- tienda/mod_tienda_products/tmpl/default.php
+++ tienda/mod_tienda_products/tmpl/default.php
@@ -12,7 +12,7 @@
 defined('_JEXEC') or die('Restricted access');
 
 // Add CSS
-$document->addStyleSheet( JURI::root(true).'/modules/mod_tienda_products/tmpl/mod_tienda_products.css');
+// $document->addStyleSheet( JURI::root(true).'/modules/mod_tienda_products/tmpl/mod_tienda_products.css');
 
 $resize = false;
 $options = array();
@@ -29,12 +29,16 @@ if ($num > 0 && @$products)
 {
 	$k = 0;
 	echo '<div class="tienda_products_'.$params->get('display_style','flat').'">';
+	echo '<div class="tienda_product_box">';
     // Loop through the products to display
     foreach (@$products as $product) : 
     	$k++;
     ?>
+
 		<div class="tienda_product_item <?php echo $params->get('moduleclass_sfx');?><?php if ($params->get('display_style','flat') == 'grid') echo ' grid' .$params->get('display_grid_items' ,'3'); ?>">
-        <?php if ($params->get('display_title','1') != '0') : ?><h4 class="product_title"><a href="<?php echo JRoute::_( $product->link."&Itemid=".$product->itemid ); ?>"><?php echo $product->product_name; ?></a></h4>
+		<div class="inner">
+        <?php if ($params->get('display_title','1') != '0') : ?>
+	<h4 class="product_title"><a href="<?php echo JRoute::_( $product->link."&Itemid=".$product->itemid ); ?>"><?php echo $product->product_name; ?></a></h4>
 		<?php endif; ?>
 		<?php if ($params->get('display_image','1') != '0') : ?>
 			<?php if ($params->get('display_image_link','1') != '0') : ?>
@@ -50,8 +54,14 @@ if ($num > 0 && @$products)
 
 		<?php if ($params->get('display_description','1') != '0' && $product->product_description_short != null) : ?><p class="product_description"><?php echo $product->product_description_short ?></p><?php endif; ?>
 		</div>
-		<?php if ($params->get('display_style','flat') == 'grid' && $params->get('display_grid_items' ,'3') == $k): ?><div style="clear:both;"></div><?php $k = 0; endif; ?>
+		</div>
+		<?php if ($params->get('display_style','flat') == 'grid' && $params->get('display_grid_items' ,'3') == $k): ?>
+			<?php echo '</div>'; ?>
+			<?php echo '<div class="tienda_product_box">'; ?>
+		<?php $k = 0; endif; ?>
+		
 		<?php  endforeach;
+	echo '</div>';
 	echo '</div>';
 
 }
@@ -60,3 +70,9 @@ if ($num > 0 && @$products)
     $text = JText::_( $null_text );
     echo $text;
 }
+?> 
+<script type="text/javascript"> 
+	window.addEvent('domready', function() {
+		$$('.tienda_product_box .tienda_product_item:last-child').addClass('right');
+	});
+</script>
