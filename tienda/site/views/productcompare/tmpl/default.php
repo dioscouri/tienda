a942284c608853a7b5607a1d09db7e53f94eb0cc
Lukas Polak <lpolak@dioscouri.com>
2011-06-06 08:55:49 +0000

    small fix of a language constant in product comparare view

--- tienda/site/views/productcompare/tmpl/default.php
+++ tienda/site/views/productcompare/tmpl/default.php
@@ -1,4 +1,4 @@
-<?php defined('_JEXEC') or die('Restricted access'); ?>
+<?php defined('_JEXEC') 	or die('Restricted access'); ?>
 <?php JHTML::_('stylesheet', 'tienda.css', 'media/com_tienda/css/'); ?>
 <?php JHTML::_('script', 'tienda.js', 'media/com_tienda/js/'); ?>
 <?php $items = @$this->items;?>
@@ -14,7 +14,7 @@
 				</td>
 					<?php foreach($items as $item):?>
 				<td align="center" valign="top" class="border-left">
-					<a title="Remove" alt="Remove" class="close-img" href="<?php echo JRoute::_('index.php?index.php?option=com_tienda&view=productcompare&task=remove&id='.$item->productcompare_id);?>">
+					<a title="<?php echo JText::_( 'Remove product comparison' ); ?>" class="close-img" href="<?php echo JRoute::_('index.php?index.php?option=com_tienda&view=productcompare&task=remove&id='.$item->productcompare_id);?>">
 						<img src="<?php echo Tienda::getURL('images');?>closebox.gif">
 					</a>
 						<?php echo TiendaHelperProduct::getImage($item->product_id, '', $item->product_name); ?>
