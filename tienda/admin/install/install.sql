8a36672906e0442cc8d37010ff290041ae6aa036
Lukas Polak <lpolak@dioscouri.com>
2011-06-07 23:24:38 +0000

    currency symbols fixed - refs #5586

--- tienda/admin/install/install.sql
+++ tienda/admin/install/install.sql
@@ -637,9 +637,9 @@ DEFAULT CHARACTER SET = utf8;
 
 INSERT IGNORE INTO `#__tienda_currencies` (`currency_id`, `currency_name`, `currency_code`, `symbol_left`, `symbol_right`, `currency_decimals`, `decimal_separator`, `thousands_separator`, `created_date`, `modified_date`, `currency_enabled`) VALUES
 (1, 'US Dollar', 'USD', '$', '', 2, '.', ',', NOW(), NOW(), 1),
-(2, 'Japanese Yen', 'JPY', 'ï¿½', '', 3, '.', ',', NOW(), NOW(), 1),
-(3, 'Euro', 'EUR', 'ï¿½', '', 2, '.', ',', NOW(), NOW(), 1),
-(4, 'British Pound', 'GBP', 'ï¿½', '', 2, '.', ',', NOW(), NOW(), 1);
+(2, 'Japanese Yen', 'JPY', '¥', '', 3, '.', ',', NOW(), NOW(), 1),
+(3, 'Euro', 'EUR', '€', '', 2, '.', ',', NOW(), NOW(), 1),
+(4, 'British Pound', 'GBP', '£', '', 2, '.', ',', NOW(), NOW(), 1);
 
 
 -- -----------------------------------------------------
