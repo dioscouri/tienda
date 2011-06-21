1ea12d9a1f42abd1209b96845ccc1918ba54a2b8
mikec_6000 <mikec_6000@dioscouri.com>
2011-06-21 22:37:59 +0000

    Report on Abandoned Carts first draft more bug to fix, refs #3508

---
+++ tienda/tienda_plugin_report_abandoned_cart/report_abandoned_cart/tmpl/form.php
@@ -0,0 +1,16 @@
+<?php defined('_JEXEC') or die('Restricted access'); ?>
+<?php JHTML::_('script', 'tienda.js', 'media/com_tienda/js/'); ?>
+<?php $state = @$vars->state; ?>
+
+    <p><?php echo JText::_( "THIS REPORTS ON DISPLAYING LIST OF USERS WITH ITEMS NOT PURCHASED" ); ?></p>
+    <div class="note">
+    	<?php echo JText::_("PRODUCT NAME"); ?>:
+			<input type="text" name="filter_name" id="filter_name" value="<?php echo @$state->filter_name; ?>" />&nbsp;&nbsp;&nbsp;
+	    <?php echo JText::_("SELECT DATE RANGE"); ?>:
+	    <?php $attribs = array('class' => 'inputbox', 'size' => '1'); ?>
+	    <?php echo TiendaSelect::reportrange( @$state->filter_range ? $state->filter_range : 'custom', 'filter_range', $attribs, 'range', true ); ?>
+	            <span class="label"><?php echo JText::_("From"); ?>:</span>
+	            <?php echo JHTML::calendar( @$state->filter_date_from, "filter_date_from", "filter_date_from", '%Y-%m-%d %H:%M:%S' ); ?>
+	            <span class="label"><?php echo JText::_("To"); ?>:</span>
+	            <?php echo JHTML::calendar( @$state->filter_date_to, "filter_date_to", "filter_date_to", '%Y-%m-%d %H:%M:%S' ); ?>
+	</div>
