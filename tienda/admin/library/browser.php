e4be6e3afc2570d91f4ad56ce9a6fbf871fb6d11
Lukas Polak <lpolak@dioscouri.com>
2011-06-21 19:33:09 +0000

    added missing defined('_JEXEC') to library/browser.php - refs #5710

--- tienda/admin/library/browser.php
+++ tienda/admin/library/browser.php
@@ -100,6 +100,9 @@
      *  + Added support for Shiretoko
      */
 
+/** ensure this file is being included by a parent file */
+defined('_JEXEC') or die('Restricted access');
+
     class TiendaBrowser 
     {
         private $_agent = '';
