36870f849c4aab4a71e1d35df0c920ccc7990761
b.nisevic <b.nisevic@dioscouri.com>
2011-08-25 14:13:39 +0000

    - changed form layout in tienda_plugin_payment_authorizedotnet, fixed #6109

--- tienda/tienda_plugin_payment_authorizedotnet/payment_authorizedotnet/tmpl/form.php
+++ tienda/tienda_plugin_payment_authorizedotnet/payment_authorizedotnet/tmpl/form.php
@@ -10,6 +10,39 @@
     <?php echo JText::_( "Tienda Authorizedotnet Payment Message" ); ?>
 </div>
 
+<?php  if (TiendaConfig::getInstance()->get('one_page_checkout')) :?>
+<table id="authorizedotnet_form">            
+    <tr>
+        <td class="field_name">
+	        <?php echo JText::_( 'Credit Card Type' ) ?><br/>
+	        <?php echo $vars->cctype_input ?>
+        </td>
+    </tr>
+    <tr>
+        <td class="field_name">
+        	<?php echo JText::_( 'Card Number' ) ?><br/>
+        	<input type="text" name="cardnum" size="35" value="<?php echo !empty($vars->prepop['x_card_num']) ? ($vars->prepop['x_card_num']) : '' ?>" />
+        </td>
+    </tr>
+    <tr>
+        <td class="field_name">
+        	<?php echo JText::_( 'Expiration Date' ) ?><br/>
+	        <?php Tienda::load( 'TiendaSelect', 'library.select' ); ?>
+	        <?php $year = date('Y'); $year_end = $year + 25; ?>
+	        <?php echo TiendaSelect::integerlist( '1', '12', '1', 'cardexp_month' ); ?>
+	        <?php echo TiendaSelect::integerlist( $year, $year_end, '1', 'cardexp_year' ); ?>	        
+	    </td>
+    </tr>
+    <tr>
+        <td class="field_name">
+        	<?php echo JText::_( 'Credit Card ID' ) ?><br/>
+            <input type="text" name="cardcvv" size="10" value="" />
+            <br/>
+            <a href="javascript:void(0);" onclick="window.open('index.php?option=com_tienda&view=checkout&task=doTask&element=payment_authorizedotnet&elementTask=showCVV&tmpl=component', '<?php echo JText::_( "Where is My Card ID" ) . "?"; ?>', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=600, height=550');"><?php echo JText::_( "Where is my card ID" ) . "?"; ?></a>
+        </td>
+    </tr>
+</table>
+<?php else: ?>
 <table id="authorizedotnet_form">            
     <tr>
         <td class="field_name"><?php echo JText::_( 'Credit Card Type' ) ?></td>
@@ -22,11 +55,10 @@
     <tr>
         <td class="field_name"><?php echo JText::_( 'Expiration Date' ) ?></td>
         <td>
-        <?php Tienda::load( 'TiendaSelect', 'library.select' ); ?>
-        <?php $year = date('Y'); $year_end = $year + 25; ?>
-        <?php echo TiendaSelect::integerlist( '1', '12', '1', 'cardexp_month' ); ?>
-        <?php echo TiendaSelect::integerlist( $year, $year_end, '1', 'cardexp_year' ); ?>
-        <?php /* <input type="text" name="cardexp" size="10" value="<?php echo !empty($vars->prepop['x_exp_date']) ? ($vars->prepop['x_exp_date']) : '' ?>" /> */ ?>
+	        <?php Tienda::load( 'TiendaSelect', 'library.select' ); ?>
+	        <?php $year = date('Y'); $year_end = $year + 25; ?>
+	        <?php echo TiendaSelect::integerlist( '1', '12', '1', 'cardexp_month' ); ?>
+	        <?php echo TiendaSelect::integerlist( $year, $year_end, '1', 'cardexp_year' ); ?>
         </td>
     </tr>
     <tr>
@@ -38,3 +70,4 @@
         </td>
     </tr>
 </table>
+<?php endif;?>
