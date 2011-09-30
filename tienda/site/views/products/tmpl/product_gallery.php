3d7f8e7c2a090ab35d24dc644f425818e343d180
Lukas Polak <lpolak@dioscouri.com>
2011-09-30 13:27:42 +0000

    fixing problems with displaying product gallery - refs #6269

--- tienda/site/views/products/tmpl/product_gallery.php
+++ tienda/site/views/products/tmpl/product_gallery.php
@@ -10,14 +10,16 @@ if ( $this->show_gallery )
 		<span><?php echo JText::_( "Images" ); ?> </span>
 	</div>
 	<?php
+	$i = 1;
 	foreach ( $this->images as $image )
 	{
 		?>
-	<div class="product_gallery_thumb" id="product_gallery_thumb">
+	<div class="product_gallery_thumb" id="product_gallery_thumb_<?php echo $i;?>">
 	<?php 
 		echo TiendaUrl::popup( $this->uri . $image, '<img src="' . $this->uri . "thumbs/" . $image . '" alt="' . $this->product_name . '" />', array( 'update' => false, 'img' => true ) ); ?>
 	</div>
 	<?php
+	$i++;
 	}
 	?>
 	<div class="reset"></div>
