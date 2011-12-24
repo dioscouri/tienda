d3c281c8184a4abc464b671b6ee4dea2b65fa2df
Chris Paschen <cpaschen@dioscouri.com>
2011-12-24 18:00:49 +0000

    Fixes more entries refs #6413

--- tienda/tienda_plugin_tool_redshopmigration/tool_redshopmigration/tmpl/form_1.php
+++ tienda/tienda_plugin_tool_redshopmigration/tool_redshopmigration/tmpl/form_1.php
@@ -103,13 +103,13 @@
                 </tr>
                 <tr>
                     <td width="100" align="right" class="key">
-                        <?php echo JText::_( 'EXTERNAL SITE URL' ); ?>:
+                        <?php echo JText::_( 'REDSHOP EXTERNAL SITE URL' ); ?>:
                     </td>
                     <td>
                         <input type="text" name="external_site_url" id="external_site_url" size="48" maxlength="250" value="<?php echo @$state->external_site_url; ?>" />
                     </td>
                     <td>
-                    	<?php echo JText::_('EXTERNAL SITE URL DESC');?>
+                    	<?php echo JText::_('REDSHOP EXTERNAL SITE URL DESC');?>
                     </td>
                 </tr>
             </table>    
