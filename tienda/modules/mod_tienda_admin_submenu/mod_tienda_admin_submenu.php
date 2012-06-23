78e12e21fb1d6bece8089714789d35399f3386af
Chris French <chris@ammonitenetworks.com>
2012-06-23 04:04:43 +0000

    the module was killing the site when you unistalled  so that is fixed.
    Everything else is working but the products add page is still really broken.

--- tienda/modules/mod_tienda_admin_submenu/mod_tienda_admin_submenu.php
+++ tienda/modules/mod_tienda_admin_submenu/mod_tienda_admin_submenu.php
@@ -10,13 +10,14 @@
 /** ensure this file is being included by a parent file */
 defined('_JEXEC') or die('Restricted access');
 
-//if (JRequest::getInt('tienda_display_submenu') == '1')
-//{
+
     // Check the registry to see if our Tienda class has been overridden
     if ( !class_exists('Tienda') ) 
     {
         JLoader::register( "Tienda", JPATH_ADMINISTRATOR.DS."components".DS."com_tienda".DS."defines.php" );
     }
+	if ( class_exists('Tienda') ) 
+    {
     Tienda::load( 'TiendaMenu', 'library.menu' );
     
     $hide = JRequest::getInt('hidemainmenu');
@@ -26,4 +27,4 @@ defined('_JEXEC') or die('Restricted access');
     $document = JFactory::getDocument();
         
     require( JModuleHelper::getLayoutPath( 'mod_tienda_admin_submenu' ) );
-//}
+	 }
