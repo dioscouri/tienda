6b32e3d6a4fbc0e173270f11b47e047e03cb0597
Chris French <chris@ammonitenetworks.com>
2012-07-07 02:49:45 +0000

    fixed bug with checkout forms, you couldn't login with joomla 2.5 on the checkout page.
    I also added a feature where you can instead of using the login forms in Tienda view, You can  just load a login module right into the view. 
    
    that way you can load in mod_login, or we can develop hybridAuth login module for tienda or  users can use their own login module

--- tienda/site/views/checkout/tmpl/form.php
+++ tienda/site/views/checkout/tmpl/form.php
@@ -19,75 +19,31 @@
                                 ' JLanguage.LOGIN_WITH_OPENID = \''.JText::_('COM_TIENDA_LOGIN_WITH_OPENID').'\';'.
                                 ' JLanguage.NORMAL_LOGIN = \''.JText::_('COM_TIENDA_NORMAL_LOGIN').'\';'.
                                 ' var modlogin = 1;';
-                $document = &JFactory::getDocument();
+                $document = JFactory::getDocument();
                 $document->addScriptDeclaration( $langScript );
                 JHTML::_('script', 'openid.js');
         endif; ?>
         
-        <form action="<?php echo JRoute::_( 'index.php', true, Tienda::getInstance()->get('usesecure', '0') ); ?>" method="post" name="login" id="form-login" >
+        <?php
         
-            <table>
-            <tr>
-                <td style="height: 40px;">
-                    <?php echo JText::_('COM_TIENDA_USERNAME'); ?> <span class>*</span>
-                </td>
-                <td>
-                    <input type="text" name="username" class="inputbox" size="18" alt="username" />
-                </td>
-            </tr>
-            <tr>
-                <td style="height: 40px;">
-                    <?php echo JText::_('COM_TIENDA_PASSWORD'); ?><span>*</span>
-                </td>
-                <td>
-                    <input type="password" name="passwd" class="inputbox" size="18" alt="password" />
-                </td>
-            </tr>
-            <?php if (JPluginHelper::isEnabled('system', 'remember')) : ?>
-            <tr>
-                <td>
-                    <?php echo JText::_('COM_TIENDA_REMEMBER_ME'); ?>
-                </td>
-                <td>
-                    <span style="float: left">
-                        <input type="checkbox" name="remember" class="inputbox" value="yes"/>
-                    </span>
-                </td>
-            </tr>
-            <?php endif; ?>
-            <tr>
-                <td>
-                </td>
-                <td style="text-align: right;">
-                    <input type="submit" name="submit" class="button" value="<?php echo JText::_('COM_TIENDA_LOGIN') ?>" />
-                </td>
-            </tr>
-            <tr>
-                <td style="height: 40px;">
-                    <ul>
-                        <li>
-                            <?php // TODO Can we do this in a lightbox or something? Why does the user have to leave? ?>
-                            <a href="<?php echo JRoute::_( 'index.php?option=com_user&view=reset' ); ?>">
-                            <?php echo JText::_('COM_TIENDA_FORGOT_YOUR_PASSWORD'); ?></a>
-                        </li>
-                        <li>
-                            <?php // TODO Can we do this in a lightbox or something? Why does the user have to leave? ?>
-                            <a href="<?php echo JRoute::_( 'index.php?option=com_user&view=remind' ); ?>">
-                            <?php echo JText::_('COM_TIENDA_FORGOT_YOUR_USERNAME'); ?></a>
-                        </li>
-                    </ul>
-                </td>
-                <td>
-                </td>
-            </tr>
-            </table>
+        $modules = JModuleHelper::getModules("tienda_checkout_login");
+		$document	= JFactory::getDocument();
+		$renderer	= $document->loadRenderer('module');
+		$attribs 	= array();
+		$attribs['style'] = 'xhtml';
+		
+		foreach ( @$modules as $mod ) 
+		{
+			echo $renderer->render($mod, $attribs);
+		}
         
-            <input type="hidden" name="option" value="com_user" />
-            <input type="hidden" name="task" value="login" />
-            <input type="hidden" name="return" value="<?php echo base64_encode( $url ); ?>" />
-            <?php echo JHTML::_( 'form.token' ); ?>
-        </form>
-    
+		
+         ?>
+        <?php if(empty($modules)) : ?>
+        
+        <?php echo  $this->loadTemplate('login'); ?>
+       
+    <?php endif; ?>
     </td>
     <td style="vertical-align: top; padding: 5px; width: 50%;">
     
