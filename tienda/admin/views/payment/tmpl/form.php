fcba11282e1e05856204f2d6908c8d7f9be1fca9
Chris French <chris@ammonitenetworks.com>
2012-07-25 00:14:47 +0000

    instead of $form->bind(); I replaced it with a foreach loop setting the values for each element which works. Now I just need to figure out why I can't load a row into jRegistery

--- tienda/admin/views/payment/tmpl/form.php
+++ tienda/admin/views/payment/tmpl/form.php
@@ -54,36 +54,30 @@
 			<fieldset>
     		<legend><?php echo JText::_('COM_TIENDA_PARAMETERS'); ?></legend>
 			<?php
-			
 			if(version_compare(JVERSION,'1.6.0','ge')) {
-			//I had to add a new XML file because the plugin XML for some reason fails to load.  try to fix this later to only need one. 	
+			//I had to add a new XML file because the plugin XML for some reason fails to load.  try to fix this later to only core plugin. 	
  			$path = JPATH_SITE.'/plugins/'.$row->folder.'/'.$row->element.'/jform/'.$row->element.'.xml';
-			
+			//ok we get a jForm Object from the new file
 			$form = JForm::getInstance("paymentplugin", $path);
-			
+			//we now need the data, so  lets use the rows params and put them in an object. 
 			$registry = new JRegistry;
-			$registry->loadString($row->params);
-			$registry->loadString('{"merchant_email":"chris@chris.com","currency":"USD2","secure_post":"0","img_url_std":"https:\/\/www.paypal.com\/en_US\/i\/btn\/x-click-but20.gif","img_url_mixed":"https:\/\/www.paypal.com\/en_US\/i\/btn\/x-click-but02.gif","sandbox":"0","sandbox_merchant_email":"","sandbox_customer_email":"","articleid":"","display_article_title":"0","failed_order_state":"10","payment_received_order_state":"17"}');
-			
+			//$registry->loadString(stripslashes($row->params));
+			//for some reason the above method for me is returning NULL, which is just stupid. if you echo $row->params and copy it and put it below it loads
+			$registry->loadString('{"merchant_email":"chris@chris.com","currency":"USD2","secure_post":"0","img_url_std":"https:\/\/www.paypal.com\/en_US\/i\/btn\/x-click-but20.gif","img_url_mixed":"https:\/\/www.paypal.com\/en_US\/i\/btn\/x-click-but02.gif","sandbox":"0","sandbox_merchant_email":"asdfasdfas@sdfasdfas.com","sandbox_customer_email":"asdfasdfasdfasdf@asdfadf.com","articleid":"12","display_article_title":"1","failed_order_state":"17","payment_received_order_state":"1"}');
 			$data = $registry->toArray();
-			$form->bind($this->data);
-			
-			
-		
-			var_dump($this->data);
-			
-			
-			
+			var_dump($data);
+			//this is supposed to bind the data, but it isn't working for me, for some reason bind is failing so We add the  values manually
+			//$bind = $form->bind($data);
+			foreach($data as $k => $v) {
+				$form->setValue($k, 'params',$v);
+			}
+			//we are actually getting fields
 			$fieldSets = $form->getFieldsets();
-
 			foreach ($fieldSets as $name => $fieldSet) :
-			
-	
 			if (isset($fieldSet->description) && trim($fieldSet->description)) :
 			echo '<p class="tip">'.$this->escape(JText::_($fieldSet->description)).'</p>';
 			endif;	
 			?>
-	
 		<?php $hidden_fields = ''; ?>
 		<ul class="adminformlist">
 			<?php foreach ($form->getFieldset($name) as $field) : ?>
@@ -99,7 +93,10 @@
 		<?php echo $hidden_fields; ?>
 	
 		<?php endforeach; ?>
-		<?php } else { ?>
+		<?php 
+		
+		
+} else { ?>
 	<?php
 	// Joomla! 1.5 code here
 	if ($output = $this -> params -> render('params')) :
