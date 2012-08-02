84f925b8393f04e8a9667d744956013b252ff71e
Chris French <chris@ammonitenetworks.com>
2012-08-02 03:55:06 +0000

    adding the view hack #7153

---
+++ tienda/admin/views/orders/tmpl/view_.php
@@ -0,0 +1,8 @@
+<?php
+	defined('_JEXEC') or die('Restricted access');
+	
+	##TODO WHY WHY WHY WHY??? for the life of me I can't figure out what Tienda is requiring view_ instead of view.'
+	
+	require_once 'view.php';
+	
+	?>
