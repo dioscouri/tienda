d5e7ae811e729ecbfa33f982f77cf41d819d9fc8
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2012-10-31 19:41:00 -0400

    Changed setHTML() to set('html', ''), fixes #15

--- tienda/site/views/checkout/tmpl/payment_options.php
+++ tienda/site/views/checkout/tmpl/payment_options.php
@@ -4,7 +4,7 @@
 <?php if( $one_page ): ?>	
 	<?php if(count($this->payment_plugins)):?>
 		<?php foreach($this->payment_plugins as $payment_plugin):?>
-		<input value="<?php echo $payment_plugin->element; ?>" onclick="tiendaGetPaymentForm('<?php echo $payment_plugin->element; ?>', 'payment_form_div', '<?php echo JText::_('COM_TIENDA_GETTING_PAYMENT_METHOD'); ?>'); $('validationmessage').setHTML(''); $('payment_form_div').addClass('note');" name="payment_plugin" type="radio" <?php echo (!empty($payment_plugin->checked)) ? "checked" : ""; ?> />
+		<input value="<?php echo $payment_plugin->element; ?>" onclick="tiendaGetPaymentForm('<?php echo $payment_plugin->element; ?>', 'payment_form_div', '<?php echo JText::_('COM_TIENDA_GETTING_PAYMENT_METHOD'); ?>'); $('validationmessage').set('html', ''); $('payment_form_div').addClass('note');" name="payment_plugin" type="radio" <?php echo (!empty($payment_plugin->checked)) ? "checked" : ""; ?> />
 		<?php echo JText::_( $payment_plugin->name ); ?>
 		<br />
 		<?php endforeach;?>
@@ -26,8 +26,8 @@
               foreach ($this->payment_plugins as $plugin) 
               {
                   ?>
-                  <input id="paymeny_<?php echo $plugin->element?>" value="<?php echo $plugin->element; ?>" onclick="tiendaGetPaymentForm('<?php echo $plugin->element; ?>', 'payment_form_div', '<?php echo JText::_('COM_TIENDA_GETTING_PAYMENT_METHOD'); ?>'); $('validationmessage').setHTML('');" name="payment_plugin" type="radio" <?php echo (!empty($plugin->checked)) ? "checked" : ""; ?> />
-                  <label for="paymeny_<?php echo $plugin->element?>" onclick="tiendaGetPaymentForm('<?php echo $plugin->element; ?>', 'payment_form_div', '<?php echo JText::_('COM_TIENDA_GETTING_PAYMENT_METHOD'); ?>'); $('validationmessage').setHTML('');"><?php echo JText::_( $plugin->name ); ?></label>
+                  <input id="paymeny_<?php echo $plugin->element?>" value="<?php echo $plugin->element; ?>" onclick="tiendaGetPaymentForm('<?php echo $plugin->element; ?>', 'payment_form_div', '<?php echo JText::_('COM_TIENDA_GETTING_PAYMENT_METHOD'); ?>'); $('validationmessage').set('html', '');" name="payment_plugin" type="radio" <?php echo (!empty($plugin->checked)) ? "checked" : ""; ?> />
+                  <label for="paymeny_<?php echo $plugin->element?>" onclick="tiendaGetPaymentForm('<?php echo $plugin->element; ?>', 'payment_form_div', '<?php echo JText::_('COM_TIENDA_GETTING_PAYMENT_METHOD'); ?>'); $('validationmessage').set('html', '');"><?php echo JText::_( $plugin->name ); ?></label>
                   <br/>
                   <?php
               }                   
