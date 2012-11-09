e326d3f1caffceb34bad44c6621dc81ce4f347f1
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2012-11-09 00:19:59 -0500

    Added a 'get shipping rates' button to OPC, fixes #24

--- tienda/site/views/checkout/tmpl/shipping_calculate.php
+++ tienda/site/views/checkout/tmpl/shipping_calculate.php
@@ -1,9 +1,10 @@
 <?php defined('_JEXEC') or die('Restricted access'); ?>
 <?php  if (!Tienda::getInstance()->get('one_page_checkout')) :?>
 	<h3><?php echo JText::_('COM_TIENDA_SELECT_A_SHIPPING_METHOD') ?></h3>
-	<input type="button" class="button" onclick="tiendaGetShippingRates( 'onCheckoutShipping_wrapper', this.form, '<?php echo JText::_('COM_TIENDA_UPDATING_SHIPPING_RATES')?>', '<?php echo JText::_('COM_TIENDA_UPDATING_CART')?>' )" value="<?php echo JText::_('COM_TIENDA_CALCULATE_SHIPPING_RATES'); ?>" />
+	<input type="button" class="button" value="<?php echo JText::_( "COM_TIENDA_GET_SHIPPING_RATES" ); ?>" onclick="tiendaGetShippingRates( 'onCheckoutShipping_wrapper', document.adminForm ); tiendaGetPaymentOptions( 'onCheckoutPayment_wrapper', document.adminForm ); " />
 <?php endif; ?>
 <input type="hidden" id="shippingrequired" name="shippingrequired" value="1"  />
 <div class="note">
 	<?php echo JText::_('COM_TIENDA_NO_SHIPPING_RATES_FOUND'); ?>
 </div>
+<input type="button" class="button" value="<?php echo JText::_( "COM_TIENDA_GET_SHIPPING_RATES" ); ?>" onclick="tiendaGetShippingRates( 'onCheckoutShipping_wrapper', document.adminForm ); tiendaGetPaymentOptions( 'onCheckoutPayment_wrapper', document.adminForm ); " />
