5ad12dc433fcda7384685b01ac6780bec83773c7
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2012-12-04 01:40:02 -0500

    Fixed offline payment plugin lang constant, fixes #143

--- tienda/plugins/tienda_plugin_payment_offline/payment_offline/tmpl/form.php
+++ tienda/plugins/tienda_plugin_payment_offline/payment_offline/tmpl/form.php
@@ -1,12 +1,12 @@
 <?php defined('_JEXEC') or die('Restricted access'); ?>
 
-<?php echo JText::_('Tienda Offline Payment Message'); ?>
+<?php echo JText::_('COM_TIENDA_OFFLINE_PAYMENT_PREPARATION_MESSAGE'); ?>
 
 <table class="adminlist">
 <tbody>
 <tr>
     <td class="key" style="width: 100px; text-align: right;">
-        <?php echo JText::_('COM_TIENDA_TIENDA_OFFLINE_PAYMENT_METHOD'); ?>
+        <?php echo JText::_('COM_TIENDA_OFFLINE_PAYMENT_METHOD'); ?>
     </td>
     <td>
         <?php echo @$vars->payment_method; ?> 
