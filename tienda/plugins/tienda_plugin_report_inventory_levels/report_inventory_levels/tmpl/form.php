31650a22cb8293a2ad3ffb8d3ef36ca2574e9438
efillman <efillman@gmail.com>
2012-12-12 21:35:08 -0500

    Changed report to filter by product name search rather than product drop down listing. Seems to be consistent with other reports.

--- tienda/plugins/tienda_plugin_report_inventory_levels/report_inventory_levels/tmpl/form.php
+++ tienda/plugins/tienda_plugin_report_inventory_levels/report_inventory_levels/tmpl/form.php
@@ -5,10 +5,9 @@
 <p><?php echo JText::_('COM_TIENDA_THIS_REPORTS_THE_INVENTORY_LEVELS'); ?></p>
 <div class="note">
 	
-	<!--<?php //echo JText::_('COM_TIENDA_PRODUCT_NAME'); ?>:
-	<input type="text" name="filter_name" id="filter_name" value="<?php //echo @$state->filter_name; ?>" />&nbsp;&nbsp;&nbsp;
-    -->
-        
+	<?php echo JText::_('COM_TIENDA_PRODUCT_NAME'); ?>:
+	<input type="text" name="filter_name" id="filter_name" value="<?php echo @$state->filter_name; ?>" />&nbsp;&nbsp;&nbsp;
+            
 	<?php echo JText::_('COM_TIENDA_SELECT_DATE_RANGE'); ?>:
     <?php $attribs = array('class' => 'inputbox', 'size' => '1', 'onchange' => 'javascript:submitbutton(\'view\').click;'); ?>
     <?php echo TiendaSelect::reportrange( @$state->filter_range ? $state->filter_range : 'custom', 'filter_range', $attribs, 'range', true ); ?>
@@ -17,7 +16,7 @@
             <span class="label"><?php echo JText::_('COM_TIENDA_TO'); ?>:</span>
             <?php echo JHTML::calendar( @$state->filter_date_to, "filter_date_to", "filter_date_to", '%Y-%m-%d %H:%M:%S' ); ?>
 	&nbsp;&nbsp;&nbsp;
-	<span><?php echo JText::_('COM_TIENDA_SELECT_PRODUCT'); ?>:</span>
-	<?php echo TiendaSelect::product( @$state->filter_product_name, 'filter_product_name', $attribs, 'product', true ); ?>
-	
+	<!--  <span><?php //echo JText::_('COM_TIENDA_SELECT_PRODUCT'); ?>:</span>
+	<?php //echo TiendaSelect::product( @$state->filter_product_name, 'filter_product_name', $attribs, 'product', true ); ?>
+	-->
 </div>
