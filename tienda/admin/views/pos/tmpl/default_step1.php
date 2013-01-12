6d437b38eb3f789221e3a248fd3eaaa842b375f8
AmmoniteNetworks <chris@ammonitenetworks.com>
2013-01-12 11:04:44 -0800

    few fixes to POS system

--- tienda/admin/views/pos/tmpl/default_step1.php
+++ tienda/admin/views/pos/tmpl/default_step1.php
@@ -17,35 +17,49 @@
           <div class="clearfix">  
             <div id="validation_message"></div>
 
-            <div class="option well well-small">
-            	<label class="radio">
-            	<input type="radio" name="user_type" value="existing" <?php if ($this->session->get('user_type', '', 'tienda_pos') == 'existing' || $this->session->get('user_type', '', 'tienda_pos') == '') { ?>checked="checked" <?php } ?> /><?php echo JText::_('COM_TIENDA_USE_AN_EXISTING_USER'); ?>
 
-            	</label>
+
+<div class="tabbable tabs-left">
+  <ul class="nav nav-tabs">
+    <li class="active"><a data-toggle="tab" href="#existing"><?php echo JText::_('COM_TIENDA_USE_AN_EXISTING_USER'); ?></a></li>
+    <li class=""><a data-toggle="tab" href="#create"><?php echo JText::_('COM_TIENDA_CREATE_A_NEW_USER'); ?></a></li>
+    <li class=""><a data-toggle="tab" href="#anonymous"><?php echo JText::_('COM_TIENDA_ANONYMOUS'); ?></a></li>
+  </ul>
+  <div class="tab-content">
+    <div class="tab-pane active" id="existing">
+             <div class="option userOption well well-small">
+                <label class="radio">
+                <input type="radio" id="radioExisting" name="user_type" value="existing" <?php if ($this->session->get('user_type', '', 'tienda_pos') == 'existing' || $this->session->get('user_type', '', 'tienda_pos') == '') { ?>checked="checked" <?php } ?> /><?php echo JText::_('COM_TIENDA_USE_AN_EXISTING_USER'); ?>
+
+                </label>
                 <div id="existing" class="option_data">
                     <?php echo $this->getModel('elementUser')->fetchElement( 'user_id', $this->session->get('user_id', '', 'tienda_pos' ) ); ?>
                     <?php echo $this->getModel('elementUser')->clearElement( 'user_id', '' ); ?>
                 </div>
-            </div>
-            
-            <div class="option well well-small">
-            	<label class="radio">
-                <input type="radio" name="user_type" value="new" <?php if ($this->session->get('user_type', '', 'tienda_pos') == 'new') { ?>checked="checked" <?php } ?> /><?php echo JText::_('COM_TIENDA_CREATE_A_NEW_USER'); ?>
+                <div class="clearfix"><br></div>
+            </div>      
+    </div>
+     <div class="tab-pane" id="create">
+            <div class="option userOption well well-small">
+                <label class="radio">
+                <input type="radio" id="radioCreate" name="user_type" value="new" <?php if ($this->session->get('user_type', '', 'tienda_pos') == 'new') { ?>checked="checked" <?php } ?> /><?php echo JText::_('COM_TIENDA_CREATE_A_NEW_USER'); ?>
                 </label>
                 <div id="new" class="option_data">
                     <input type="text" name="new_email" value="<?php echo $this->session->get('new_email', JText::_('COM_TIENDA_EMAIL'), 'tienda_pos' ); ?>" size="40" onclick="tiendaClearInput( this, '<?php echo JText::_('COM_TIENDA_EMAIL'); ?>' );" />
                     <input type="text" name="new_name" value="<?php echo $this->session->get('new_name', JText::_('COM_TIENDA_FULLNAME'), 'tienda_pos' ); ?>" size="75" onclick="tiendaClearInput( this, '<?php echo JText::_('COM_TIENDA_FULLNAME'); ?>' );" />
                 </div>
                 <div class="option_data">
-                    <input type="checkbox" name="new_username_create" value="yes" checked="checked" />
-                    <?php echo JText::_('COM_TIENDA_AUTO_CREATE_USERNAME'); ?>
-                    <input type="text" name="new_username" value="<?php echo $this->session->get('new_username', JText::_('COM_TIENDA_USERNAME'), 'tienda_pos' ); ?>" size="40" onclick="tiendaClearInput( this, '<?php echo JText::_('COM_TIENDA_USERNAME'); ?>' );" />
+                    <input id="new_username_create" type="checkbox" name="new_username_create" value="yes" checked="checked" /><?php echo JText::_('COM_TIENDA_AUTO_CREATE_USERNAME'); ?>
+                   <div id="createUsernameHolder" style="display:none;"> 
+                    <input type="text"  name="new_username" value="<?php echo $this->session->get('new_username', JText::_('COM_TIENDA_USERNAME'), 'tienda_pos' ); ?>" size="40" onclick="tiendaClearInput( this, '<?php echo JText::_('COM_TIENDA_USERNAME'); ?>' );" />
+                    </div>
                 </div>
-            </div>
-            
-            <div class="option well well-small">
-            	<label class="radio">
-                <input type="radio" name="user_type" value="anonymous" <?php if ($this->session->get('user_type', '', 'tienda_pos') == 'anonymous') { ?>checked="checked" <?php } ?> /><?php echo JText::_('COM_TIENDA_ANONYMOUS'); ?>
+            </div>        
+    </div>
+     <div class="tab-pane" id="anonymous">
+             <div class="option userOption well well-small">
+                <label class="radio">
+                <input type="radio" id="radioAnno" name="user_type" value="anonymous" <?php if ($this->session->get('user_type', '', 'tienda_pos') == 'anonymous') { ?>checked="checked" <?php } ?> /><?php echo JText::_('COM_TIENDA_ANONYMOUS'); ?>
                 </label>
                 <div id="anonymous" class="option_data">
                     <input type="checkbox" name="anon_emails" value="yes" />
@@ -56,13 +70,46 @@
                         <input type="text" name="anon_email" value="<?php echo $this->session->get('anon_email', JText::_('COM_TIENDA_EMAIL'), 'tienda_pos' ); ?>" size="40" onclick="tiendaClearInput( this, '<?php echo JText::_('COM_TIENDA_EMAIL'); ?>' );" />
                     </div>
                 </div>
-            </div>
+            </div>    
+    </div>
+  </div>
+</div>
+
+           
+            
+            
+            
+            
             
             <div class="continue">
                 <?php $onclick = "tiendaValidation( '" . $this->validation_url . "', 'validation_message', 'saveStep1', document.adminForm, true, '".JText::_('COM_TIENDA_VALIDATING')."' );"; ?> 
                 <input onclick="<?php echo $onclick; ?>" value="<?php echo JText::_('COM_TIENDA_CONTINUE_STEP1'); ?>" type="button" class="button btn btn-success" />
             </div>
         </div>
-        
+ 
+<script>
+//this is just for testing, could probably be better when moved to remote JS
+jQuery(document).ready(function() {
+    jQuery('a[data-toggle="tab"]').on('shown', function (e) {
+  e.target // activated tab
+  e.relatedTarget // previous tab
+  // uncheck all radio buttons, and than find and check the radio button in active tab
+  var id = jQuery(e.target).attr('href');
+  jQuery('input:radio[name=user_type]').attr('checked',false);
+  jQuery(id + ' input:radio').attr('checked',true);
+})
+    jQuery('#new_username_create').click(function() {
+        jQuery('#createUsernameHolder').toggle();
+    }) 
+
+});
+
+
+
+
+
+
+</script>
+
 
 <input type="hidden" name="nextstep" id="nextstep" value="step2" />
